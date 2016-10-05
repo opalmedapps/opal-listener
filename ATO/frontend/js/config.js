db75a63e63799143d0a8bb08b86cdb80e0bd0429
Ackeem J <ackeem.berry@gmail.com>
2016-10-05 12:17:05 -0400

    Deleted frontend scripts (temporary)

--- ATO/frontend/js/config.js
+++
@@ -1,7 +0,0 @@
-/* Define global variables here */
-
-var ABSPATH = "/var/www/devDocuments/ATO/"; // dev
-var URLPATH = "/devDocuments/ATO/"; // dev
-
-//var ABSPATH = "/var/www/Documents/AEHRA/"; // pro
-//var URLPATH = "/Documents/AEHRA/"; // pro
