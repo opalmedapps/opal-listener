a12a0b98135e482820d17b8f060a6bb029a267ee
Robert Maglieri <robert.maglieri@gmail.com>
2016-12-13 15:20:48 -0500

    Changed DB to QPlusApp on 39 server

--- listener/credentials.js
+++ listener/credentials.js
@@ -1,7 +1,7 @@
 var exports=module.exports={};
 exports.MYSQL_USERNAME='root';
 exports.MYSQL_PASSWORD='service';
-exports.MYSQL_DATABASE='OpalDB_DEV';
+exports.MYSQL_DATABASE='QPlusApp';
 //exports.FIREBASE_SECRET='wX6RW5wPthYjEuUxfAviFa2BwXPdjuLEVcCIV7dB';
 //exports.FIREBASE_REQUEST_URL='https://opal-dev.firebaseio.com/dev2/requests';
 //exports.FIREBASE_URL='https://opal-dev.firebaseio.com/dev2';
