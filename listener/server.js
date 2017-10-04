0330750cfbf51aad2c2eae872e51fe0aaa38895c
James Brace <bracejames95@gmail.com>
2017-10-04 10:17:40 -0400

    fix logging in issue

--- listener/server.js
+++ listener/server.js
@@ -60,7 +60,7 @@ listenForRequest('passwordResetRequests');
 function listenForRequest(requestType){
     logger.log('debug','Starting '+ requestType+' listener.');
     ref.child(requestType).on('child_added', function(snapshot){
-        logger.info("received request:" + JSON.stringify(snapshot));
+        logger.info("received request:" + snapshot));
         handleRequest(requestType,snapshot);
     });
 }
