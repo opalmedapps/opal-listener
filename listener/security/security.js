ea470b384c9ba5b9e9944cc388b925b8b3941825
James Brace <bracejames95@gmail.com>
2017-11-02 13:53:07 -0400

    fix blocking concurrent users + ssn validation testing

--- listener/security/security.js
+++ listener/security/security.js
@@ -69,7 +69,10 @@ exports.verifySecurityAnswer=function(requestKey,requestObject,patient)
     //If its the right security answer, also make sure is a valid SSN;
     var response = {};
 
-    var isVerified = (unencrypted.PasswordReset == true)? unencrypted.SSN && unencrypted.SSN.toUpperCase() === patient.SSN && unencrypted.Answer && unencrypted.Answer === patient.AnswerText: unencrypted.Answer === patient.AnswerText;
+    var isVerified = (unencrypted.PasswordReset == true) ? unencrypted.SSN && unencrypted.SSN.toUpperCase() === patient.SSN && unencrypted.Answer && unencrypted.Answer === patient.AnswerText: unencrypted.Answer === patient.AnswerText;
+
+    console.log("ssn validation: " + unencrypted.SSN && unencrypted.SSN.toUpperCase() === patient.SSN && unencrypted.Answer && unencrypted.Answer === patient.AnswerText);
+
 
     if (isVerified) {
         response = { RequestKey:requestKey, Code:3,Data:{AnswerVerified:"true"}, Headers:{RequestKey:requestKey,RequestObject:requestObject},Response:'success'};
