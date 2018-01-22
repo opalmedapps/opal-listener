1f8c1e621b2f510735a7bdb7007c157b48bb6f2b
James Brace <bracejames95@gmail.com>
2018-01-22 14:52:04 -0500

    Fix checkCheckin

--- listener/api/apiPatientUpdate.js
+++ listener/api/apiPatientUpdate.js
@@ -65,7 +65,13 @@ exports.refresh = function (requestObject) {
 //Check checkin API call
 exports.checkCheckin = function(requestObject)
 {
-  return sqlInterface.checkCheckinInAria(requestObject);
+    var r = Q.defer();
+
+    sqlInterface.checkCheckin(requestObject.Parameters.PatientSerNum)
+        .then(function(hasAttempted){ r.resolve({Data: { AttemptedCheckin: hasAttempted}}) })
+        .catch(function(err){ r.reject(err) });
+
+    return r.promise;
 };
 
 //Get checkin update API call
