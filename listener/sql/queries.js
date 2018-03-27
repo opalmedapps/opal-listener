11b84ca571c9983144c8d18a5bbea9ad2531f016
David Herrera <davidfherrerar@gmail.com>
2018-03-27 14:25:42 -0400

    --- Removed the extra field in Answered Questionnaire table    --- Modified the query for the lab results

--- listener/sql/queries.js
+++ listener/sql/queries.js
@@ -149,6 +149,19 @@ exports.patientTestResultsTableFields=function()
 {
 
 	return 'SELECT ComponentName, FacComponentName, AbnormalFlag, MaxNorm, MinNorm, TestValue, TestValueString, UnitDescription, CAST(TestDate AS char(30)) as `TestDate`, ' +
+				'IfNull((Select EMC.URL_EN From EducationalMaterialControl EMC, TestResultExpression TRE, TestResultControl TRC ' +
+					'Where EMC.EducationalMaterialControlSerNum = TRC.EducationalMaterialControlSerNum ' +
+						'and TRE.TestResultControlSerNum = TRC.TestResultControlSerNum ' +
+						'and TRE.TestResultExpressionSerNum = TR.TestResultExpressionSerNum), "") as URL_EN, ' +
+				'IfNull((Select EMC.URL_FR From EducationalMaterialControl EMC, TestResultExpression TRE, TestResultControl TRC ' +
+					'Where EMC.EducationalMaterialControlSerNum = TRC.EducationalMaterialControlSerNum ' +
+						'and TRE.TestResultControlSerNum = TRC.TestResultControlSerNum ' +
+						'and TRE.TestResultExpressionSerNum = TR.TestResultExpressionSerNum), "") as URL_FR ' +
+				'FROM TestResult TR, Users U, Patient P ' +
+				'WHERE P.AccessLevel = 3 AND U.UserTypeSerNum=P.PatientSerNum AND TR.PatientSerNum = P.PatientSerNum AND TR.TestDate >= "1970-01-01" AND U.Username LIKE ? AND TR.LastUpdated > ? AND TR.ValidEntry = "Y";';
+
+/*
+	return 'SELECT ComponentName, FacComponentName, AbnormalFlag, MaxNorm, MinNorm, TestValue, TestValueString, UnitDescription, CAST(TestDate AS char(30)) as `TestDate`, ' +
 					'IfNull((Select EMC.URL_EN From EducationalMaterialControl EMC, TestResultControl TRC ' +
 									'Where EMC.EducationalMaterialControlSerNum = TRC.EducationalMaterialControlSerNum ' +
 											'and TRC.TestResultControlSerNum = TR.TestResultControlSerNum), "") as URL_EN, ' +
@@ -157,7 +170,7 @@ exports.patientTestResultsTableFields=function()
 											'and TRC.TestResultControlSerNum = TR.TestResultControlSerNum), "") as URL_FR ' +
 				'FROM TestResult TR, Users U, Patient P ' +
 				'WHERE P.AccessLevel = 3 AND U.UserTypeSerNum=P.PatientSerNum AND TR.PatientSerNum = P.PatientSerNum AND TR.TestDate >= "1970-01-01" AND U.Username LIKE ? AND TR.LastUpdated > ? AND TR.ValidEntry = "Y";';
-/**/
+*/
 
 /*
 return 'SELECT ComponentName, FacComponentName, AbnormalFlag, MaxNorm, MinNorm, TestValue, TestValueString, UnitDescription, CAST(TestDate AS char(30)) as `TestDate`, ' +
