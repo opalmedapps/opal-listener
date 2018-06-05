d8b0b2599ce160c5fc72a466f26a3159b46fa41e
yickmo <yickmo@gmail.com>
2018-06-05 09:00:50 -0400

    Removed deleted appointments

--- listener/sql/queries.js
+++ listener/sql/queries.js
@@ -56,7 +56,6 @@ exports.patientAppointmentsTableFields=function()
         "" +
         "INNER JOIN Users U ON U.UserTypeSerNum = P.PatientSerNum " +
         "INNER JOIN Appointment Appt ON Appt.PatientSerNum = P.PatientSerNum " +
-        // "INNER JOIN HospitalMap HM ON HM.HospitalMapSerNum = Appt.Location " +
         "INNER JOIN ResourceAppointment RA ON RA.AppointmentSerNum = Appt.AppointmentSerNum " +
         "INNER JOIN Resource R ON RA.ResourceSerNum = R.ResourceSerNum " +
         "INNER JOIN AliasExpression AE ON AE.AliasExpressionSerNum=Appt.AliasExpressionSerNum " +
@@ -68,7 +67,8 @@ exports.patientAppointmentsTableFields=function()
         "WHERE " +
         "U.Username = ? " +
         "AND Appt.State = 'Active' " +
-        // "AND (Appt.LastUpdated > ? OR A.LastUpdated > ? OR AE.LastUpdated > ? OR R.LastUpdated > ? OR HM.LastUpdated > ?) " +
+        "AND Appt.Status <> 'Deleted' " +
+        "AND (Appt.LastUpdated > ? OR A.LastUpdated > ? OR AE.LastUpdated > ? OR R.LastUpdated > ? OR HM.LastUpdated > ?) " +
         "" +
         "ORDER BY Appt.AppointmentSerNum, ScheduledStartTime;";
 
