0b16402662c1a0d5e40d1d1fe4e2bcb764624859
Arthur A. Bergamaschi <arthurbergmz@gmail.com>
2018-09-05 16:03:01 -0400

    :construction: Added get Appointments History by appointment comparison.

--- listener/api/modules/waitingTimeVisualization/request.js
+++ listener/api/modules/waitingTimeVisualization/request.js
@@ -1,7 +1,15 @@
 module.exports = function (requestObject) {
   return new Promise((resolve, reject) => {
+    const { refId, refSource } = requestObject.Parameters
+    console.log('')
+    console.log('')
+    console.log('')
     console.log('WaitingTimeVisualization requested!')
-    console.log(requestObject)
-    resolve({hello: 'world'})
+    console.log(refId, refSource)
+    console.log('')
+    console.log('')
+    console.log('')
+    
+    resolve({delays: {Set1: 0, Set2: 0, Set3: 0, Set4: 0}})
   })
 }
