859a2f16118200ea1c5d216f3dea9721e5a880ae
Arthur A. Bergamaschi <arthurbergmz@gmail.com>
2018-09-11 16:01:06 -0400

    :rocket: Waiting Time Visualizations ready.

--- listener/api/modules/waitingTimeVisualization/request.js
+++ listener/api/modules/waitingTimeVisualization/request.js
@@ -4,53 +4,39 @@ const cacheOf = require('./cache')
 
 function createDefaultErrorLogger (cb) {
   return (err) => {
-    for(let i = 0; i < 10; ++i) console.log('')
-    console.log('===== END OF WAITING TIME VISUALIZATION BECAUSE OF ERROR =====')
     logger.log('error', err)
     cb({data: {err: JSON.stringify(err.stack || err)}})
   }
 }
 
 function onDataReady (cb, appointmentType, scheduledDay, scheduledHour, scheduledMinutes) {
-  return (sets) => {
-    for(let i = 0; i < 10; ++i) console.log('')
-    console.log('===== END OF WAITING TIME VISUALIZATION =====')
-    cb({data: {delays: JSON.stringify({sets, appointmentType, scheduledDay, scheduledHour, scheduledMinutes})}})
-  }
+  return (sets) => cb({data: {delays: JSON.stringify({sets, appointmentType, scheduledDay, scheduledHour, scheduledMinutes})}})
 }
 
 module.exports = function (requestObject) {
-  console.log('===== BEGINNING OF WAITING TIME VISUALIZATION =====')
-  for(let i = 0; i < 10; ++i) console.log('')
   return new Promise((resolve) => {
     const { refId, refSource } = requestObject.Parameters
-    console.log('refId: ', refId)
-    console.log('refSource: ', refSource)
     const onError = createDefaultErrorLogger(resolve)
     if (!refId) {
-      return onError(resolve)(new Error(`Missing parameter on request: "refId".`))
+      return onError(new Error(`Missing parameter on request: "refId".`))
     }
     if (!refSource) {
-      return onError(resolve)(new Error('Missing parameter on request: "refSource".'))
+      return onError(new Error('Missing parameter on request: "refSource".'))
     }
     const processor = retrieveProcessor(refSource)
     if (!processor) {
-      return onError(resolve)(new Error(`Unknown source: ${source}.`))
+      return onError(new Error(`Unknown source: ${refSource}.`))
     }
-    console.log('Processor found!')
     processor.getAppointment(refId)
       .then(({appointmentType, scheduledDay, scheduledHour, scheduledMinutes}) => {
-        console.log('appointmentData: ', appointmentType, scheduledDay, scheduledHour, scheduledMinutes)
         const cache = cacheOf(appointmentType, scheduledDay, scheduledHour, scheduledMinutes)
         cache.verify()
           .then((cachedSets) => {
-            console.log('cachedSets: ', cachedSets)
             if(cachedSets) {
               return onDataReady(resolve, appointmentType, scheduledDay, scheduledHour, scheduledMinutes)(cachedSets)
             }
             processor.process(appointmentType, scheduledDay, scheduledHour, scheduledMinutes)
               .then(({set1, set2, set3, set4}) => {
-                console.log('processed sets: ', set1, set2, set3, set4)
                 cache.save(set1, set2, set3, set4)
                   .then(onDataReady(resolve, appointmentType, scheduledDay, scheduledHour, scheduledMinutes))
                   .catch(onError)
