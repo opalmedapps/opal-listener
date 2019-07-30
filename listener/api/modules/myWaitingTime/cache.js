f976f3eb424f5deeabdb3e0e5119233ad7092b6f
Tessa Condon <tesseract970@gmail.com>
2019-07-30 12:16:07 -0400

    Remove Comments and Notes from Code

--- listener/api/modules/myWaitingTime/cache.js
+++ listener/api/modules/myWaitingTime/cache.js
@@ -4,9 +4,7 @@ const logger = require('./../../../logs/logger.js')
 
 module.exports = function (patientId) {
   return new Promise((resolve, reject) => {
-//Tessa
     runSqlQuery(opalDbQueries.createTable())
-    //endTessa
     .then(function(){return runSqlQuery(opalDbQueries.getTimestamps(),[patientId])})
       .then((results) => resolve(results && results.length > 0 ? results : []))
       .catch(reject)
