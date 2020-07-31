faf7eb788fa2021870700b015914235e4f868ae6
Yuan Chen <19421218+YC000@users.noreply.github.com>
2020-07-31 11:59:14 -0400

    remove waiting time feature (#18)
    Remove all code related to the waiting time functionality as per the pen test suggestions.

--- listener/api/modules/waiting-time/appointmentDelays/queries/index.js
+++
@@ -1,9 +0,0 @@
-const opalDb = require('./opalDb')
-const waitingRoomManagement = require('./waitingRoomManagement')
-
-const _default = {
-  opalDb,
-  waitingRoomManagement
-}
-
-module.exports = _default
