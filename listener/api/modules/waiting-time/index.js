e6374cad384ed31642c3dc37609fbc5c0628dd2e
Yuan Chen <19421218+YC000@users.noreply.github.com>
2020-07-31 11:59:14 -0400

    remove waiting time feature (#18)
    Remove all code related to the waiting time functionality as per the pen test suggestions.

--- listener/api/modules/waiting-time/index.js
+++
@@ -1,9 +0,0 @@
-const myWaitingTime = require('./myWaitingTime');
-const appointmentDelays = require('./appointmentDelays');
-
-const _default = {
-  myWaitingTime,
-  appointmentDelays
-};
-
-module.exports = _default;
