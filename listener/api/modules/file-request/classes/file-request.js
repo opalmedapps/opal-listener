33cdb18913bb693bdb75d7f9f2ce0b801c1832ad
Stacey Beard <staceybeard10@gmail.com>
2021-08-20 11:57:31 -0400

    Fixed pdf file downloads on Android 11
      - Due to stronger security policies in the app, pdf downloads are now handled in the listener. See new module "file-request".

---
+++ listener/api/modules/file-request/classes/file-request.js
@@ -0,0 +1,39 @@
+const request = require('request').defaults({ encoding: null });  // 'encoding: null' allows conversion to base64
+
+/**
+ * @description Class representing a request to a file from the internet.
+ */
+class FileRequest {
+
+    /**
+     * @param url The url of the file to request.
+     */
+    constructor(url) {
+        this._url = url;
+    }
+
+    /**
+     * @author Stacey Beard
+     * @date 2021-08-20
+     * @description Fetches a file from the internet (at this._url) and returns the content in base64 encoding.
+     *              Source: https://stackoverflow.com/questions/17124053/node-js-get-image-from-web-and-encode-with-base64
+     * @returns {Promise<unknown>} Resolves to a data object containing the base64 content and content-type.
+     */
+    getFileBase64() {
+        return new Promise((resolve, reject) => {
+            request.get(this._url, function (error, response, body) {
+                if (error) reject(error);
+                else if (response.statusCode === 200) {
+                    let data = {
+                        contentType: response.headers["content-type"],
+                        base64Data: Buffer.from(body).toString('base64'),
+                    };
+                    resolve(data);
+                }
+                else reject(`Request returned with a response status other than '200 OK': status = ${response.statusCode}`);
+            });
+        });
+    }
+}
+
+module.exports = {FileRequest};
---
+++ listener/api/modules/file-request/request-handlers/file.request-handler.js
@@ -0,0 +1,36 @@
+const {ApiRequestHandler} = require("../../../api-request-handler");
+const {FileRequest} = require("../classes/file-request");
+const logger = require("../../../../logs/logger");
+const {param} = require("express-validator");
+const {ValidationError} = require("../../../errors/validation-error");
+
+class FileRequestHandler extends ApiRequestHandler {
+
+    static validators = [
+        param("url", "Must provide valid url")
+            .exists()
+            .isString()
+            .notEmpty(),
+    ];
+
+    static async handleRequest(requestObject) {
+
+        // Validate the request
+        const errors = await FileRequestHandler.validate(requestObject.parameters);
+        if (!errors.isEmpty()) {
+            logger.log("error", "Validation Error", errors);
+            throw new ValidationError(errors);
+        }
+
+        logger.log("info", `Requesting file from: ${requestObject.parameters.url}`);
+
+        // Create a FileRequest object to process the request
+        const file = new FileRequest(requestObject.parameters.url);
+
+        return {
+            "data": await file.getFileBase64(),
+        }
+    }
+}
+
+module.exports = FileRequestHandler;
