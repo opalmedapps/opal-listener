012f9c2e3a93d39538ca2495cfc9874867998ded
Matthias Schoettle <3097098-mschoettle@users.noreply.gitlab.com>
2022-05-19 13:30:47 +0000

    fix: Add option to allow unauthorized certificates for SMTP (QSCCD-204)
    See merge request opalmedapps/opal-listener!85

--- legacy-registration/api/utility/mail.js
+++ legacy-registration/api/utility/mail.js
@@ -9,6 +9,11 @@ function sendMail(config, recipient, subject, text) {
             user: config.username,
             pass: config.password,
         },
+        // Allow configuration of unauthorized CAs (such as self-signed certificates)
+        // See: https://github.com/nodemailer/nodemailer/issues/406#issuecomment-83941225
+        tls: {
+            rejectUnauthorized: config.tls_reject_unauthorized,
+        },
     });
 
     let message = {
