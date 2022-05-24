ebfddad8fc714857eee44c2cb859bcffee71c8e3
David Gagne <davidgagne85@gmail.com>
2022-05-24 18:24:49 +0000

    feature: Add request to new backend
    * Adjustments following code review
    
    * Fix typo
    
    * Adjustment following code review and architechture discussion
    
    * Adjustments following code review
    
    * Modified request-handler to use most of the new structure
    
    * Moved config to `config.json` and modified make request accordingly.
    
    * Made request to new backend works
    
    * Made request to new backend api
    
    * Adjustment following code review
    
    * Adjustments following code review
    
    * Fixed typo in method description
    
    * Added encryption to request simulator
    
    * Create simulate request class to help testing
    
    See merge request opalmedapps/opal-listener#83

--- src/core/request-handler.test.js
+++ src/core/request-handler.test.js
@@ -3,14 +3,21 @@ const { expect } = require('chai');
 const { RequestHandler } = require('./request-handler');
 
 describe('Request Handler', function () {
-    describe('#processSecurityRequest()', function () {
-        it('Should reject promise when trying to process security request', function () {
-            return expect(RequestHandler.processSecurityRequest()).to.be.rejected;
+    describe('validateSnapshot', function () {
+        it('Should return false if snapshot as no key', function () {
+            return expect(RequestHandler.validateSnapshot({ Request: '' })).to.be.false;
+        });
+        it('Should return false when snapshot is empty', function () {
+            return expect(RequestHandler.validateSnapshot({})).to.be.false;
+        });
+        it('Should return false when snapshot is undefined', function () {
+            return expect(RequestHandler.validateSnapshot(undefined)).to.be.false;
         });
     });
-    describe('#processRequest()', function () {
-        it('Should reject promise when trying to process regular request', function () {
-            return expect(RequestHandler.processRequest()).to.be.rejected;
+    describe('processRequest()', function () {
+        it('Should throw error when trying to process request when Firebase is not initialize', async function () {
+            const requestHandler = new RequestHandler({});
+            return expect(await requestHandler.processRequest('api', {})).to.throw;
         });
     });
 });
