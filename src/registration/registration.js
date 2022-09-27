0de4cf3c1319230fd36643701f18aa2bc44e8f69
Napparific <kelly.agnew@mail.mcgill.ca>
2022-09-27 10:10:43 -0400

    refactor: adjust logger messaging

--- src/registration/registration.js
+++ src/registration/registration.js
@@ -2,6 +2,7 @@ const Keyv = require('keyv');
 const ApiRequest = require('../core/api-request');
 const legacyLogger = require('../../listener/logs/logger');
 // TODO: Replace config.json with environment variable (DATA_CACHE_TIME_TO_LIVE_MINUTES) when QSCCD-207 is merged.
+//       Sub: Add TTL var to env validation in environment.js (QSCCD-207 as well)
 const config = require('../config/config.json');
 
 const regCache = new Keyv({ namespace: 'registration' });
@@ -20,13 +21,16 @@ class Registration {
         };
         // Retrieve encryption values from memory if TTL has not been reached
         if (!await regCache.get('salt') && !await regCache.get('secret')) {
-            legacyLogger.log('info', 'DATACACHE TTL EXPIRED, FETCHING NEW DATA');
+            legacyLogger.log('info', 'Registration encryption datacache TTL expired; fetching new data');
             const response = await ApiRequest.makeRequest(requestParams);
             await regCache.set('salt', response.data.patient.ramq, config.DATA_CACHE_TIME_TO_LIVE_MINUTES * 60 * 1000);
             await regCache.set('secret', response.data.code, config.DATA_CACHE_TIME_TO_LIVE_MINUTES * 60 * 1000);
-        } // TODO: Reset clock on lookup
+        }
         else {
-            legacyLogger.log('info', 'LOADING DATA FROM CACHE');
+            legacyLogger.log('info', 'Loading registration encryption data from cache and resetting ttl');
+            await regCache.set('salt', await regCache.get('salt'), config.DATA_CACHE_TIME_TO_LIVE_MINUTES * 60 * 1000);
+            // eslint-disable-next-line max-len
+            await regCache.set('secret', await regCache.get('secret'), config.DATA_CACHE_TIME_TO_LIVE_MINUTES * 60 * 1000);
         }
         // TODO handle decryption using MRNs
         // https://o-hig.atlassian.net/browse/QSCCD-427
