0dc09e5caca23431389934933ad47b2ce5a572c4
Kelly Agnew <kelly.agnew@mail.mcgill.ca>
2022-10-04 17:16:14 +0000

    QSCCD-445: Cache encryption data in-memory for subsequent requests
    Closes QSCCD-445
    
    * fix: update logging message, change boolean logic in api call branch
    
    * rebase: update enviornment validation with ttl var
    
    * rebase: replace config vars with environment
    
    * fix: instance specific salt & secret for datacache
    
    * refactor: adjust logger messaging
    
    * refactor: remove original data cache implementation
    
    * rebase: remove old config
    
    * fix: add space
    
    * test: add test class for registrations
    
    * feat: try keyv package to resolve async/await issues with class implementation
    
    * feat: initial implementation of data caching class
    
    See merge request opalmedapps/opal-listener!120

--- src/registration/registration.js
+++ src/registration/registration.js
@@ -1,7 +1,20 @@
+const Keyv = require('keyv');
 const ApiRequest = require('../core/api-request');
+const legacyLogger = require('../../listener/logs/logger');
 
+const regCache = new Keyv({ namespace: 'registration' });
+regCache.on('error', err => legacyLogger.log(
+    'error',
+    'KeyV registration data cache error',
+    err,
+));
 class Registration {
     static async getEncryptionValues(snapshot) {
+        // We bind the snapshot branch name to dedicated salt and secret keys for this regCache instance
+        // This allows to keep a dedicated datacache for each unique registration code hash
+        const instanceSalt = `salt-${snapshot.BranchName}`;
+        const instanceSecret = `secret-${snapshot.BranchName}`;
+
         const requestParams = {
             Parameters: {
                 method: 'get',
@@ -11,12 +24,39 @@ class Registration {
                 },
             },
         };
-        const response = await ApiRequest.makeRequest(requestParams);
+        // Retrieve encryption values from memory if TTL has not been reached
+        if (!await regCache.get(instanceSalt) || !await regCache.get(instanceSecret)) {
+            legacyLogger.log('info', 'Registration encryption datacache TTL expired; fetching new data');
+            const response = await ApiRequest.makeRequest(requestParams);
+            await regCache.set(
+                instanceSalt,
+                response.data.patient.ramq,
+                process.env.DATA_CACHE_TIME_TO_LIVE_MINUTES * 60 * 1000,
+            );
+            await regCache.set(
+                instanceSecret,
+                response.data.code,
+                process.env.DATA_CACHE_TIME_TO_LIVE_MINUTES * 60 * 1000,
+            );
+        }
+        else {
+            legacyLogger.log('info', 'Loading registration encryption data from cache and resetting ttl');
+            await regCache.set(
+                instanceSalt,
+                await regCache.get(instanceSalt),
+                process.env.DATA_CACHE_TIME_TO_LIVE_MINUTES * 60 * 1000,
+            );
+            await regCache.set(
+                instanceSecret,
+                await regCache.get(instanceSecret),
+                process.env.DATA_CACHE_TIME_TO_LIVE_MINUTES * 60 * 1000,
+            );
+        }
         // TODO handle decryption using MRNs
         // https://o-hig.atlassian.net/browse/QSCCD-427
         return {
-            salt: response.data.patient.health_insurance_number,
-            secret: response.data.code,
+            salt: await regCache.get(instanceSalt),
+            secret: await regCache.get(instanceSecret),
         };
     }
 }
