d53d6306f40050c1733eee8fdd9b48b8fe7252e7
Limin Liu <limin.liu@muhc.mcgill.ca>
2022-11-09 16:30:39 +0000

    Feat add api calls add and get firebase user to listener (QSCCD-666)
    Closes QSCCD-666
    
    * feat deps fix the log
    
    * feat deps fix the log
    
    * feat deps refactor the code
    
    * feat deps refactor the code
    
    * feat deps refactor the apis
    
    * feat deps remove the code
    
    * feat deps add api calls add and get firebase user to listener
    
    See merge request opalmedapps/opal-listener#125

--- legacy-registration/api/firebase/firebaseFunctions.js
+++ legacy-registration/api/firebase/firebaseFunctions.js
@@ -2,72 +2,30 @@
 
 const admin = require("firebase-admin");
 const logger = require('../../logs/logger.js');
-const Q = require('q');
-const config = require('../../config-adaptor');
 
 
 // Function to create firebase account
-exports.createFirebaseAccount = function (userEmail, userPassword) {
-	let r = Q.defer();
-
-    admin.auth().createUser({
+exports.createFirebaseAccount = async function (userEmail, userPassword) {
+    const response = await admin.auth().createUser({
         email: userEmail,
         password: userPassword
-    })
-    .then(function (userRecord) {
-        // Firebase account has been created and it returns the firebase unique ID
-        logger.log('debug','Successfully created new firebase user account and this is the unique Id: ' + userRecord.uid);
-        r.resolve(userRecord.uid);
-    })
-    .catch(function (error) {
-        logger.log('error', 'Error while creating new user account in firebase: ' + error.code);
-        r.reject(error.errorInfo);
     });
-
-    return r.promise;
+    if (response?.uid) {
+        logger.log('debug',`Successfully created new firebase user account and this is the unique Id: ${response.uid}`);
+    }
+    return response?.uid;
 };
 
-exports.deleteFirebaseAccount = function (uid) {
-    let r = Q.defer();
-
-    admin.auth().deleteUser(uid)
-        .then(function () {
-            logger.log('debug', 'Successfully delete the firebase account using passed uniqueID: ' + uid);
-            r.resolve();
-        })
-        .catch(function (error) {
-            logger.log('Error', 'Error while deleting user account in firebase' + error);
-            r.reject();
-
-        });
-    return r.promise;
-
+// Function to create firebase account
+exports.getFirebaseAccountByEmail = async function (userEmail) {
+    const response = await admin.auth().getUserByEmail(userEmail);
+    if (response?.uid) {
+        logger.log('debug', `Successfully got firebase user account and this is the unique Id: ${response.uid}`);
+    }
+    return response?.uid;
 };
-//const serviceAccount = require(config.FIREBASE_ADMIN_KEY);
 
-//admin.initializeApp({
-//    credential: admin.credential.cert(serviceAccount),
-//    databaseURL: config.DATABASE_URL
-//});
-
-//// Enable firebase logging
-//admin.database.enableLogging(false);
-
-//// Get reference to correct data element
-//const db = admin.database();
-//const ref = db.ref(config.firebaseBranch.parentBranch);
-
-
-//admin.auth().createUser({
-//    email: 'test@test.com',
-//    password: 'TEst'
-//})
-//    .then(function (userRecord) {
-//        // Firebase account has been created and it returns the firebase unique ID
-//        logger.log('debug', 'Successfully created new firebase user account and this is the unique Id: ' + userRecord.uid);
-//        r.resolve(userRecord.uid);
-//    })
-//    .catch(function (error) {
-//        logger.log('Error', 'Error while creating new user account in firebase' + error.code);
-//        r.resolve(error.code);
-//    });
+exports.deleteFirebaseAccount = async function (uid) {
+    await admin.auth().deleteUser(uid)
+    logger.log('debug', `Successfully delete the firebase account using passed uniqueID: ${uid}`);
+};
