f6363781c4284a3b8c3bbb236ebafaad989d6893
Limin Liu <limin.liu@muhc.mcgill.ca>
2022-11-21 21:40:07 +0000

    feat: insert patient in register patient (QSCCD-668)
    Closes QSCCD-668
    
    * feat deps remove file require
    
    * feat deps add return asserts
    
    * fix deps revert the code
    
    * feat deps refactor the test function add more assert info
    
    * feat deps add exception test function
    
    * feat deps fix env file
    
    * feat deps fix env file
    
    * feat deps fix and refactor the test function
    
    * feat deps fix package-lock
    
    * feat deps update lock json
    
    * feat deps refactor the code
    
    * feat deps add test function for registration request api
    
    * feat deps refactor the code
    
    * feat deps refactor the code
    
    * feat deps refactor the code
    
    * fix deps fix typo
    
    * feat deps refactor the code
    
    * feat deps refactor the code
    
    * feat deps insert patient and patient hospital identifier in register patient
    
    * feat deps remove comments
    
    * feat deps refactor the code
    
    * feat deps add api retrieve patient detailed data from new backend
    
    See merge request opalmedapps/opal-listener#127

---
+++ legacy-registration/api/request/request.test.js
@@ -0,0 +1,87 @@
+const { expect } = require('chai');
+const axios = require('axios');
+const sinon = require('sinon');
+const opalRequest = require('./request');
+const ApiRequest = require('../../../src/core/api-request');
+
+describe('opalRequest.retrievePatientDataDetailed', function () {
+	it('retrievePatientDataDetailed success', async function () {
+		sinon.stub(ApiRequest, 'makeRequest')
+			.returns(Promise.resolve({
+				data: {
+					hospital_patients: [
+						{
+							mrn: '9999993',
+							site_code: 'aaa',
+						},
+					],
+					patient: {
+						date_of_birth: '1985-01-01',
+						first_name: 'Kevin',
+						last_name: 'Chen',
+						ramq: 'TESC53511613',
+						sex: 'M',
+					}
+				},
+			}));
+
+		const requestObject = {
+			Parameters: {
+				Fields: {},
+			},
+		};
+		requestObject.Parameters.Fields.registrationCode = 'A0127Q0T50hk';
+		requestObject.Parameters.Fields.language = 'en';
+		const response = await opalRequest.retrievePatientDataDetailed(requestObject);
+
+		expect(response).to.have.property('patient');
+		expect(response.patient).to.have.property('ramq').equal('TESC53511613');
+		expect(response).to.have.property('hospital_patients');
+		expect(response.hospital_patients.length).equal(1);
+		const expectedParameters = {
+			Parameters: {
+				method: 'get',
+				url: `/api/registration/${requestObject.Parameters.Fields.registrationCode}/?detailed`,
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept-Language': requestObject.Parameters.Fields.language,
+				}
+			}
+		};
+		sinon.assert.calledWith(ApiRequest.makeRequest, expectedParameters);
+		sinon.assert.calledOnce(ApiRequest.makeRequest);
+
+		ApiRequest.makeRequest.restore();
+	});
+
+	it('retrievePatientDataDetailed failed', async function () {
+		sinon.stub(ApiRequest, 'makeRequest').throws(new Error('API_NOT_FOUND'));
+
+		const requestObject = {
+			Parameters: {
+				Fields: {},
+			},
+		};
+		requestObject.Parameters.Fields.registrationCode = 'CODE12345678';
+		requestObject.Parameters.Fields.language = 'en';
+		try {
+			await opalRequest.retrievePatientDataDetailed(requestObject);
+		} catch (error) {
+			expect(error.message).to.equal('API_NOT_FOUND');
+			const expectedParameters = {
+				Parameters: {
+					method: 'get',
+					url: `/api/registration/${requestObject.Parameters.Fields.registrationCode}/?detailed`,
+					headers: {
+						'Content-Type': 'application/json',
+						'Accept-Language': requestObject.Parameters.Fields.language,
+					}
+				}
+			};
+			sinon.assert.calledWith(ApiRequest.makeRequest, expectedParameters);
+			sinon.assert.calledOnce(ApiRequest.makeRequest);
+		}
+
+		ApiRequest.makeRequest.restore();
+	});
+});
