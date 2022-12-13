adf6dbb9cdf9023e0645cedf545b0fde8b6533ec
Limin Liu <limin.liu@muhc.mcgill.ca>
2022-12-13 19:46:17 +0000

    Feat Add api to get lab result history data (QSCCD-696)
    * feat deps refactor the apis
    
    * feat deps fix default language
    
    * feat deps refactor the test functions
    
    * feat deps refactor the apis
    
    * feat deps refactor the api annotation and test function
    
    * feat deps refactor the api getLabResultHistory
    
    * feat deps refactor the code
    
    * feat deps add api to get lab result history
    
    * feat deps refactor the code
    
    * feat deps remove file require
    
    * feat deps add return asserts
    
    * fix deps revert the code
    
    * feat deps refactor the test function add more assert info
    
    * feat deps add exception test function
    
    * feat deps fix env file
    
    * feat deps fix env file
    
    * feat deps fix and refactor the test function
    
    * feat deps fix package-lock
    
    * feat deps update lock json
    
    * feat deps refactor the code
    
    * feat deps add test function for registration request api
    
    * feat deps refactor the code
    
    * feat deps refactor the code
    
    * feat deps add internal function calling new backend api registration register
    
    * feat deps refactor the code
    
    * feat deps refactor the code
    
    * fix deps fix typo
    
    * feat deps refactor the code
    
    * feat deps refactor the code
    
    * feat deps insert patient and patient hospital identifier in register patient
    
    * feat deps remove comments
    
    * feat deps refactor the code
    
    * feat deps add api retrieve patient detailed data from new backend
    
    See merge request opalmedapps/opal-listener#133

--- legacy-registration/api/request/request.test.js
+++ legacy-registration/api/request/request.test.js
@@ -1,87 +1,206 @@
 const { expect } = require('chai');
-const axios = require('axios');
 const sinon = require('sinon');
 const opalRequest = require('./request');
-const ApiRequest = require('../../../src/core/api-request');
 
-describe('opalRequest.retrievePatientDataDetailed', function () {
-	it('retrievePatientDataDetailed success', async function () {
-		sinon.stub(ApiRequest, 'makeRequest')
-			.returns(Promise.resolve({
-				data: {
-					hospital_patients: [
-						{
-							mrn: '9999993',
-							site_code: 'aaa',
-						},
-					],
-					patient: {
-						date_of_birth: '1985-01-01',
-						first_name: 'Kevin',
-						last_name: 'Chen',
-						ramq: 'TESC53511613',
-						sex: 'M',
-					}
-				},
-			}));
+describe('opalRequest', function () {
+	describe('retrievePatientDataDetailed', function () {
+		it('retrievePatientDataDetailed success', async function () {
+			sinon.stub(opalRequest, 'axiosApi')
+				.returns(Promise.resolve({
+					data: {
+						hospital_patients: [
+							{
+								mrn: '9999993',
+								site_code: 'aaa',
+							},
+						],
+						patient: {
+							date_of_birth: '1985-01-01',
+							first_name: 'Kevin',
+							last_name: 'Chen',
+							ramq: 'TESC53511613',
+							sex: 'M',
+						}
+					},
+				}));
 
-		const requestObject = {
-			Parameters: {
-				Fields: {},
-			},
-		};
-		requestObject.Parameters.Fields.registrationCode = 'A0127Q0T50hk';
-		requestObject.Parameters.Fields.language = 'en';
-		const response = await opalRequest.retrievePatientDataDetailed(requestObject);
+			const requestObject = {
+				registrationCode: 'A0127Q0T50hk',
+				language: 'fr',
+			};
+			const response = await opalRequest.retrievePatientDataDetailed(requestObject);
 
-		expect(response).to.have.property('patient');
-		expect(response.patient).to.have.property('ramq').equal('TESC53511613');
-		expect(response).to.have.property('hospital_patients');
-		expect(response.hospital_patients.length).equal(1);
-		const expectedParameters = {
-			Parameters: {
-				method: 'get',
-				url: `/api/registration/${requestObject.Parameters.Fields.registrationCode}/?detailed`,
-				headers: {
-					'Content-Type': 'application/json',
-					'Accept-Language': requestObject.Parameters.Fields.language,
-				}
-			}
-		};
-		sinon.assert.calledWith(ApiRequest.makeRequest, expectedParameters);
-		sinon.assert.calledOnce(ApiRequest.makeRequest);
-
-		ApiRequest.makeRequest.restore();
-	});
-
-	it('retrievePatientDataDetailed failed', async function () {
-		sinon.stub(ApiRequest, 'makeRequest').throws(new Error('API_NOT_FOUND'));
-
-		const requestObject = {
-			Parameters: {
-				Fields: {},
-			},
-		};
-		requestObject.Parameters.Fields.registrationCode = 'CODE12345678';
-		requestObject.Parameters.Fields.language = 'en';
-		try {
-			await opalRequest.retrievePatientDataDetailed(requestObject);
-		} catch (error) {
-			expect(error.message).to.equal('API_NOT_FOUND');
+			expect(response).to.have.property('patient');
+			expect(response.patient).to.have.property('ramq').equal('TESC53511613');
+			expect(response).to.have.property('hospital_patients');
+			expect(response.hospital_patients.length).equal(1);
 			const expectedParameters = {
-				Parameters: {
+                method: 'get',
+                url: `${process.env.OPAL_BACKEND_HOST}/api/registration/${requestObject.registrationCode}/?detailed`,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept-Language': requestObject.language,
+                    'Authorization': opalRequest.backendToken,
+                    'Appuserid': opalRequest.appuserid,
+                }
+			};
+			sinon.assert.calledWith(opalRequest.axiosApi, expectedParameters);
+			sinon.assert.calledOnce(opalRequest.axiosApi);
+
+			opalRequest.axiosApi.restore();
+		});
+
+		it('retrievePatientDataDetailed failed', async function () {
+			sinon.stub(opalRequest, 'axiosApi').throws(new Error('Calling retrievePatientDataDetailed failed'));
+
+			const requestObject = {
+				registrationCode: 'CODE12345678',
+				language: 'fr',
+			};
+			try {
+				await opalRequest.retrievePatientDataDetailed(requestObject);
+			} catch (error) {
+				expect(error.message).to.equal('Calling retrievePatientDataDetailed failed');
+				const expectedParameters = {
 					method: 'get',
-					url: `/api/registration/${requestObject.Parameters.Fields.registrationCode}/?detailed`,
+					url: `${process.env.OPAL_BACKEND_HOST}/api/registration/${requestObject.registrationCode}/?detailed`,
 					headers: {
 						'Content-Type': 'application/json',
-						'Accept-Language': requestObject.Parameters.Fields.language,
+						'Accept-Language': requestObject.language,
+						'Authorization': opalRequest.backendToken,
+						'Appuserid': opalRequest.appuserid,
 					}
-				}
-			};
-			sinon.assert.calledWith(ApiRequest.makeRequest, expectedParameters);
-			sinon.assert.calledOnce(ApiRequest.makeRequest);
-		}
+				};
+				sinon.assert.calledWith(opalRequest.axiosApi, expectedParameters);
+				sinon.assert.calledOnce(opalRequest.axiosApi);
+			}
 
-		ApiRequest.makeRequest.restore();
+			opalRequest.axiosApi.restore();
+		});
+	});
+
+	describe('registrationRegister', function () {
+		it('registrationRegister success', async function () {
+			sinon.stub(opalRequest, 'axiosApi')
+				.returns(Promise.resolve({
+					data: {},
+				}));
+
+			const request = {
+				registrationCode: 'A0127Q0T50hk',
+				language: 'en',
+			}
+			const registerData = {
+				patient: {
+					legacy_id: '1'
+				},
+				caregiver: {
+					language: 'en',
+					phone_number: '+15142223333',
+				},
+				security_answers: [
+					{
+						question: 'pet',
+						answer: 'bird',
+					},
+				],
+			}
+
+			const response = await opalRequest.registrationRegister(request, registerData);
+			expect(response).to.empty;
+
+			const expectedParameters = {
+				method: 'post',
+				url: `${process.env.OPAL_BACKEND_HOST}/api/registration/${request.registrationCode}/register/`,
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept-Language': request.language,
+					'Authorization': opalRequest.backendToken,
+					'Appuserid': opalRequest.appuserid,
+				},
+				data: registerData,
+			};
+			sinon.assert.calledWith(opalRequest.axiosApi, expectedParameters);
+			sinon.assert.calledOnce(opalRequest.axiosApi);
+
+			opalRequest.axiosApi.restore();
+		});
+
+		it('registrationRegister failed', async function () {
+			sinon.stub(opalRequest, 'axiosApi').throws(new Error('Calling registrationRegister failed'));
+
+			const request = {
+				registrationCode: 'A0127Q0T50hk',
+				language: 'en',
+			}
+			const registerData = {
+				patient: {
+					legacy_id: '1'
+				},
+				caregiver: {
+					language: 'en',
+					phone_number: '+15142223333',
+				},
+				security_answers: [
+					{
+						question: 'pet',
+						answer: 'bird',
+					},
+				],
+			}
+
+			try {
+				await await opalRequest.registrationRegister(request, registerData);
+			} catch (error) {
+				expect(error.message).to.equal('Calling registrationRegister failed');
+				const expectedParameters = {
+					method: 'post',
+					url: `${process.env.OPAL_BACKEND_HOST}/api/registration/${request.registrationCode}/register/`,
+					headers: {
+						'Content-Type': 'application/json',
+						'Accept-Language': request.language,
+						'Authorization': opalRequest.backendToken,
+						'Appuserid': opalRequest.appuserid,
+					},
+					data: registerData,
+				};
+				sinon.assert.calledWith(opalRequest.axiosApi, expectedParameters);
+				sinon.assert.calledOnce(opalRequest.axiosApi);
+			}
+
+			opalRequest.axiosApi.restore();
+		});
+	});
+
+	describe('getLabResultHistory', function () {
+		it('getLabResultHistory success', async function () {
+			sinon.stub(opalRequest, 'axiosApi')
+				.returns(Promise.resolve({
+					data: {},
+				}));
+
+			const labResultHistoryURL = 'https://test_url.com'
+			const requestData = {
+				PatientId: '1',
+				Site: '1',
+			}
+
+			const response = await opalRequest.getLabResultHistory(labResultHistoryURL, requestData);
+			expect(response).to.empty;
+
+			const requestParams = {
+				method: 'post',
+				url: labResultHistoryURL,
+				headers: {'Content-Type': 'application/json'},
+				data: {
+					json: true,
+					body: requestData,
+				},
+			}
+			sinon.assert.calledWith(opalRequest.axiosApi, requestParams);
+			sinon.assert.calledOnce(opalRequest.axiosApi);
+
+			opalRequest.axiosApi.restore();
+		});
 	});
 });
