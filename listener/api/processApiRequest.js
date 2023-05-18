84105198c162581a9f64203341f60536f7e09e84
Stacey Beard <staceybeard10@gmail.com>
2023-05-18 17:40:21 -0400

    chore: mark 'Login' request as deprecated

--- listener/api/processApiRequest.js
+++ listener/api/processApiRequest.js
@@ -17,6 +17,7 @@ const LEGACYAPI = {
     'DeviceIdentifier': apiHospitalUpdate.updateDeviceIdentifier,
     'Log': apiPatientUpdate.logActivity,
     'LogPatientAction': apiPatientUpdate.logPatientAction,
+    // Deprecated API entry: 'Login'
     'Login': apiPatientUpdate.login,
     'Logout': apiPatientUpdate.logout,
     'UserPatient': apiPatientUpdate.getUserPatient,
