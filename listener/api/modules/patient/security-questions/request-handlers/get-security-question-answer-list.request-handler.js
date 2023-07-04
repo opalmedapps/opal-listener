84e34ab14f1fcffb13bbc102b715cb1685c23bfe
Shifeng Chen <shifeng.chen@muhc.mcgill.ca>
2023-07-04 14:39:54 +0000

    fix: replace updating security answer queries to call backend API
    See merge request opalmedapps/opal-listener#183

--- listener/api/modules/patient/security-questions/request-handlers/get-security-question-answer-list.request-handler.js
+++ listener/api/modules/patient/security-questions/request-handlers/get-security-question-answer-list.request-handler.js
@@ -1,30 +1,78 @@
 const {ApiRequestHandler} = require("../../../../api-request-handler");
 const {Patient} = require("../../patient");
-const {PatientSecurityQuestion} = require("../classes/patient-security-question");
+const SecurityDjango = require("../../../../../security/securityDjango");
+const {Version} = require('../../../../../../src/utility/version');
+const logger = require("../../../../../logs/logger");
 
 class GetSecurityQuestionAnswerListRequestHandler extends ApiRequestHandler{
     /**
-     * Handler for the SecurityQuestionAnswerList request, returns 2 lists for security questions related to a given patient
+     * Handler for the SecurityQuestionAnswerList request, returns 2 list for security questions related to the current user
      * @param {OpalRequest} requestObject
-     * @returns {Promise<{data: {patientSerNum: number, activeSecurityQuestionList: [], securityQuestionWithAnswerList: []}}>}
+     * @returns {Promise<{data: {patientSerNum: number, securityQuestionList: [], activeSecurityQuestions: []}}>}
      */
     static async handleRequest(requestObject){
         const patient = await Patient.getPatientByUsername(requestObject.meta.UserID);
-        const patientSecurityQuestion = new PatientSecurityQuestion(patient);
 
+        let activeSecurityQuestions;
         let securityQuestionList;
-        let securityQuestionWithAnsList;
+        let apiResponse;
+        let activeQuestionForLowerVersion = [];
+        let securityQuestionForLowerVersion = [];
+        let questionIdObj = {}
 
-        [securityQuestionList, securityQuestionWithAnsList] =
-            await Promise.all([patientSecurityQuestion.getActiveSecurityQuestionList(), patientSecurityQuestion.getSecurityAnswerList()]);
+        apiResponse = await SecurityDjango.getSecurityQuestionList(requestObject.meta.UserID);
+        if (apiResponse.length === 0) throw "API call returned a empty list of questions for the current user";
+        securityQuestionList = apiResponse.results;
 
-        return {
-            "data": {
-                "patientSerNum": patient.patientSerNum,
-                "securityQuestionWithAnswerList": securityQuestionWithAnsList,
-                "activeSecurityQuestionList": securityQuestionList,
-            }
-        };
+        apiResponse = await SecurityDjango.getActiveSecurityQuestions();
+        if (apiResponse.length === 0) throw "API call returned a empty list of all the active questions";
+        activeSecurityQuestions = apiResponse.results;
+
+        // Getting security question list format read by the app has changed after 1.12.2
+        if (Version.versionLessOrEqual(requestObject.meta.AppVersion, Version.version_1_12_2)) {
+            activeSecurityQuestions.forEach(questionObj => {
+                activeQuestionForLowerVersion.push(
+                    {
+                        "SecurityQuestionSerNum": questionObj.id,
+                        "QuestionText_EN": questionObj.title_en,
+                        "QuestionText_FR": questionObj.title_fr,
+                        "Active": 1
+                    }
+                );
+                questionIdObj[questionObj.title_en] = questionObj.id;
+                questionIdObj[questionObj.title_fr] = questionObj.id;
+            });
+
+            securityQuestionList.forEach(securityQuestionObj => {
+                let questionId = -1;
+                if (questionIdObj[securityQuestionObj.question] !== "undefined") questionId = questionIdObj[securityQuestionObj.question];
+                securityQuestionForLowerVersion.push(
+                    {
+                        "SecurityAnswerSerNum": securityQuestionObj.id,
+                        "QuestionText_EN": securityQuestionObj.question,
+                        "QuestionText_FR": securityQuestionObj.question,
+                        "Active": 1,
+                        "SecurityQuestionSerNum": questionId
+                    }
+                );
+            });
+            return {
+                "data": {
+                    "patientSerNum": patient.patientSerNum,
+                    "securityQuestionWithAnswerList": securityQuestionForLowerVersion,
+                    "activeSecurityQuestionList": activeQuestionForLowerVersion,
+                }
+            };
+        }
+        else{
+            return {
+                "data": {
+                    "patientSerNum": patient.patientSerNum,
+                    "securityQuestionList": securityQuestionList,
+                    "activeSecurityQuestions": activeSecurityQuestions,
+                }
+            };
+        }
     }
 }
 
--- listener/security/securityDjango.js
+++ listener/security/securityDjango.js
@@ -31,6 +31,132 @@ class SecurityDjango {
             throw new Error('Failed to get random security question and answer from the backend');
         }
     }
+
+    /**
+     * @desc Calls the backend to get all the active security questions.
+     * @returns {Promise<any>} Resolves to get all the active security questions.
+     */
+    static async getActiveSecurityQuestions() {
+        const requestParams = {
+            Parameters: {
+                method: 'get',
+                url: `/api/security-questions/`,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            },
+        };
+        logger.log('info', "API: Calling backend to get all the active security questions");
+        const response = await ApiRequest.makeRequest(requestParams);
+        if (response?.data) return response.data;
+        else {
+            logger.log('error', 'Error from API call', response);
+            throw new Error('Failed to get all the active security questions from the backend');
+        }
+    }
+
+    /**
+     * @desc Calls the backend to get a specific active security question from SecurityQuestion model.
+     * @param {int} questionId The security question id.
+     * @returns {Promise<any>} Resolves to a specific active security questions.
+     */
+    static async getSpecificActiveSecurityQuestion(questionId) {
+        if (!questionId) throw new Error('Cannot call API; no questionId value provided');
+        const requestParams = {
+            Parameters: {
+                method: 'get',
+                url: `/api/security-questions/${questionId}/`,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            },
+        };
+        logger.log('info', "API: Calling backend to get a specific active security question");
+        const response = await ApiRequest.makeRequest(requestParams);
+        if (response?.data) return response.data;
+        else {
+            logger.log('error', 'Error from API call', response);
+            throw new Error('Failed to get a specific active security question from the backend');
+        }
+    }
+
+    /**
+     * @desc Calls the backend to get a list of security questions for the current user.
+     * @param {string} userId The Firebase username of the user making the request.
+     * @returns {Promise<any>} Resolves to a list of security questions for the current user.
+     */
+    static async getSecurityQuestionList(userId) {
+        if (!userId) throw new Error('Cannot call API; no userId value provided');
+        const requestParams = {
+            Parameters: {
+                method: 'get',
+                url: `/api/caregivers/${userId}/security-questions/`,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            },
+        };
+        logger.log('info', "API: Calling backend to get a list of security questions for the user");
+        const response = await ApiRequest.makeRequest(requestParams);
+        if (response?.data) return response.data;
+        else {
+            logger.log('error', 'Error from API call', response);
+            throw new Error('Failed to get a list of security questions from the backend');
+        }
+    }
+
+    /**
+     * @desc Calls the backend to get a specific security question from SecurityAnswer model for the current user.
+     * @param {string} userId The Firebase username of the user making the request.
+     * @param {int} questionId The security question id.
+     * @returns {Promise<any>} Resolves to a specific security questions for the current user.
+     */
+    static async getSpecificSecurityQuestion(userId, questionId) {
+        if (!userId) throw new Error('Cannot call API; no userId value provided');
+        if (!questionId) throw new Error('Cannot call API; no questionId value provided');
+        const requestParams = {
+            Parameters: {
+                method: 'get',
+                url: `/api/caregivers/${userId}/security-questions/${questionId}/`,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            },
+        };
+        logger.log('info', "API: Calling backend to get a specific security question for the user");
+        const response = await ApiRequest.makeRequest(requestParams);
+        if (response?.data) return response.data;
+        else {
+            logger.log('error', 'Error from API call', response);
+            throw new Error('Failed to get a specific security question from the backend');
+        }
+    }
+
+    /**
+     * @desc Calls the backend to update security answers for the questions user provided.
+     * @param {string} userId The Firebase username of the user making the request.
+     * @param {array} questionAnswerArr an array containing objects with properties: questionId, question, answer
+     */
+    static async updateSecurityQuestionAndAnswerList(userId, questionAnswerArr) {
+        if (!userId) throw new Error('Cannot call API; no userId value provided');
+        questionAnswerArr.forEach(function(questionAnswerObj) {
+            const requestParams = {
+                Parameters: {
+                    method: 'put',
+                    url: `/api/caregivers/${userId}/security-questions/${questionAnswerObj.questionId}/`,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    data: {
+                        'question': questionAnswerObj.question,
+                        'answer': questionAnswerObj.answer,
+                    }
+                },
+            };
+            ApiRequest.makeRequest(requestParams);
+        });
+        logger.log('info', "API: Calling backend to update security answers for the questions user provided");
+    }
 }
 
 module.exports = SecurityDjango;
