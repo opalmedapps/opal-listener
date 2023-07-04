087c5b44435bd95ddbacb855f44377d3a9072817
Shifeng Chen <shifeng.chen@muhc.mcgill.ca>
2023-07-04 14:39:54 +0000

    fix: replace updating security answer queries to call backend API
    See merge request opalmedapps/opal-listener!183

--- listener/api/modules/patient/security-questions/request-handlers/update-security-question-answer.request-handler.js
+++ listener/api/modules/patient/security-questions/request-handlers/update-security-question-answer.request-handler.js
@@ -1,9 +1,12 @@
 const {ApiRequestHandler} = require("../../../../api-request-handler");
 const {Patient} = require("../../patient");
-const {PatientSecurityQuestion} = require("../classes/patient-security-question");
 const {ValidationError} = require("../../../../errors/validation-error");
+const {OpalSQLQueryRunner} = require("../../../../../sql/opal-sql-query-runner");
+const opalQueries = require('../../../../../sql/queries');
 const logger = require("../../../../../logs/logger");
 const {param} = require("express-validator");
+const SecurityDjango = require("../../../../../security/securityDjango");
+const {Version} = require('../../../../../../src/utility/version');
 
 class UpdateSecurityQuestionAnswerRequestHandler extends ApiRequestHandler {
     /**
@@ -13,14 +16,14 @@ class UpdateSecurityQuestionAnswerRequestHandler extends ApiRequestHandler {
         param("questionAnswerArr", "Must provide valid questionAnswerArr parameter")
             .exists()
             .isArray(),
-        param("questionAnswerArr.*.questionSerNum", "Must provide valid questionSerNum property")
+        param("questionAnswerArr.*.question", "Must provide valid question property")
             .exists()
-            .isNumeric(),
+            .isString(),
         param("questionAnswerArr.*.answer", "Must provide valid answer property")
             .exists()
             .isString()
             .notEmpty(),
-        param("questionAnswerArr.*.securityAnswerSerNum", "Must provide valid securityAnswerSerNum property")
+        param("questionAnswerArr.*.questionId", "Must provide valid questionId property")
             .exists()
             .isNumeric(),
     ];
@@ -31,17 +34,47 @@ class UpdateSecurityQuestionAnswerRequestHandler extends ApiRequestHandler {
      * @returns {Promise<{data: {Response: string, patientSerNum: number}}>}
      */
     static async handleRequest(requestObject) {
-
-        const errors = await UpdateSecurityQuestionAnswerRequestHandler.validate(requestObject.parameters);
-        if (!errors.isEmpty()) {
-            logger.log("error", "Validation Error", errors);
-            throw new ValidationError(errors.errors);
+        // Only validate the parameters return from the app when the version is after 1.12.2
+        if (Version.versionGreaterThan(requestObject.meta.AppVersion, Version.version_1_12_2)) {
+            const errors = await UpdateSecurityQuestionAnswerRequestHandler.validate(requestObject.parameters);
+            if (!errors.isEmpty()) {
+                logger.log("error", "Validation Error", errors);
+                throw new ValidationError(errors.errors);
+            }
         }
 
         const patient = await Patient.getPatientByUsername(requestObject.meta.UserID);
-        const patientSecurityQuestion = new PatientSecurityQuestion(patient);
-
-        await patientSecurityQuestion.updateSecurityQuestionAndAnswer(requestObject.parameters.questionAnswerArr);
+        const Language = (await OpalSQLQueryRunner.run(opalQueries.patientTableFields(), [patient.patientSerNum]))[0].Language;
+        let questionAnswerForLowerVersion = [];
+        let questionAnswerArr = requestObject.parameters.questionAnswerArr;
+        // Updating security question and answer array format read by the app has changed after 1.12.2
+        if (Version.versionLessOrEqual(requestObject.meta.AppVersion, Version.version_1_12_2)) {
+            let apiResponse
+            let question
+            for (var key in questionAnswerArr) {
+                if (Object.hasOwn(questionAnswerArr[key], "questionSerNum") && (questionAnswerArr[key].questionSerNum !== null) && (questionAnswerArr[key].questionSerNum !== undefined)){
+                    // Call Backend API to retrieve question from SecurityQuestion model when the active question is selected and updated
+                    apiResponse = await SecurityDjango.getSpecificActiveSecurityQuestion(questionAnswerArr[key].questionSerNum);
+                    question = (Language == 'EN') ? apiResponse.title_en : apiResponse.title_fr;
+                }
+                else{
+                    // Call Backend API to retrieve question from SecurityAnswer model
+                    apiResponse = await SecurityDjango.getSpecificSecurityQuestion(requestObject.meta.UserID, questionAnswerArr[key].securityAnswerSerNum);
+                    question = apiResponse.question
+                }
+                questionAnswerForLowerVersion.push({
+                    question: question,
+                    questionId: questionAnswerArr[key].securityAnswerSerNum,
+                    answer: questionAnswerArr[key].answer
+                });
+            }
+            // reformat the array value to mathc data structure before and equal version 1.12.2
+            await SecurityDjango.updateSecurityQuestionAndAnswerList(requestObject.meta.UserID, questionAnswerForLowerVersion);
+        }
+        // new data structure after version 1.12.2
+        else{
+            await SecurityDjango.updateSecurityQuestionAndAnswerList(requestObject.meta.UserID, questionAnswerArr);
+        }
 
         return {
             "data": {
