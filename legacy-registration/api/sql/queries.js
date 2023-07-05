e7d0a712a09d8f7bd3041dd55d65caece97ed4ac
Stacey Beard <stacey.beard@muhc.mcgill.ca>
2023-07-05 22:49:11 +0000

    fix: update RegisterPatient to support non-self registration
    Made the necessary changes to `RegisterPatient` to support registering for a non-self relationship.
    
    * Broke down the monolithic `RegisterPatient` function into separate steps to make it easier to follow.
    * Moved a large portion of the registration SQL from stored procedures or DB functions to the queries file.
    * Broke down DB inserts into separate steps that can be executed conditionally:
      - A new patient row is inserted if the patient doesn't exist yet.
      - A dummy patient row for the caregiver (needed for some legacy functionality) is inserted if the caregiver is new and is not registering for a self relationship.
      - A new user row is inserted if the user doesn't exist yet.
    
    See merge request opalmedapps/opal-listener!186

--- legacy-registration/api/sql/queries.js
+++ legacy-registration/api/sql/queries.js
@@ -1,10 +1,49 @@
-exports.updatePatient = function () {
-    return "SELECT reg_UpdatePatientInfo(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) AS Result;";
+exports.insertPatient = () => {
+    return `INSERT INTO Patient(FirstName, LastName, Sex, DateOfBirth, Age, Email, Language, SSN, AccessLevel, RegistrationDate)
+            VALUES (?, ?, ?, ?, TIMESTAMPDIFF(year, ?, now()), ?, ?, ?, ?, NOW());
+    `;
 };
 
-exports.insertPatient = () => {
-    return `INSERT INTO Patient(FirstName, LastName, Sex, DateOfBirth, Age, TelNum, EnableSMS, Email, Language, SSN)
-            VALUES (?, ?, ?, ?, TIMESTAMPDIFF(year, ?, now()), ?, 0, ?, ?, ?);
+/**
+ * @description Query to insert a mostly empty dummy row in the Patient table, used as a necessary "self" placeholder
+ *              for Caregivers who don't have a self relationship.
+ * @returns {string} The query.
+ */
+exports.insertDummyPatient = () => {
+    return `INSERT INTO Patient(FirstName, LastName, Sex, DateOfBirth, Email, Language, SSN)
+            VALUES (?, ?, 'Unknown', '0000-00-00 00:00:00', ?, ?, '');
+    `;
+};
+
+exports.insertUser = () => {
+    return `INSERT INTO Users (UserType, UserTypeSerNum, Username, Password, SessionId)
+            VALUES (?, ?, ?, ?, '');
+    `;
+};
+
+/**
+ * @description Query to update the fields relevant to the "self" user in the Patient table.
+ * @returns {string} The query.
+ */
+exports.updateSelfPatientInfo = () => {
+    return `UPDATE Patient
+            SET
+                Email = ?,
+                Language = ?,
+                TelNum = ?,
+                EnableSMS = 0,
+                AccessLevel = ?,
+                RegistrationDate = NOW(),
+                ConsentFormExpirationDate = DATE_ADD(NOW(), INTERVAL 1 YEAR),
+                TermsAndAgreementSign = ?,
+                TermsAndAgreementSignDateTime = NOW()
+            WHERE PatientSerNum = ?;
+    `;
+}
+
+exports.insertPatientControl = () => {
+    return `INSERT IGNORE INTO PatientControl (PatientSerNum)
+            VALUES (?);
     `;
 }
 
@@ -12,6 +51,11 @@ exports.insertPatientHospitalIdentifier = function () {
     return "SELECT reg_insertPatientHospitalIdentifier(?,?,?) AS Result;";
 };
 
-exports.getSiteAndMrn = function() {
-    return "SELECT PHI.MRN AS Mrn, PHI.Hospital_Identifier_Type_Code AS Site FROM Patient_Hospital_Identifier AS PHI INNER JOIN Patient AS P ON PHI.PatientSerNum = P.PatientSerNum AND P.SSN = ?;";
-};
+exports.getPatientSerNumFromUserSerNum = function () {
+    return `SELECT p.PatientSerNum
+            FROM Patient p, Users u
+            WHERE u.UserTypeSerNum = p.PatientSerNum
+              AND u.UserSerNum = ?
+            ;
+    `;
+}
