1c909d072bc4753b10174dca6ad65b25d225e785
Stacey Beard <stacey.beard@muhc.mcgill.ca>
2023-08-09 18:34:41 +0000

    fix: add a temporary patch to alleviate check-in race condition issue in prod
    See merge request opalmedapps/opal-listener!193

--- listener/api/sqlInterface.js
+++ listener/api/sqlInterface.js
@@ -413,7 +413,8 @@ exports.checkIn = async function (requestObject) {
             }
             // Check whether the check-in call succeeded, or all attempts failed
             // On a success, return all checked-in appointments to the app
-            if (success) return await getCheckedInAppointments(patientSerNum);
+            // TODO: once the check-in race condition is fixed, delete the function getCheckedInAppointmentsLoop and change this line to call getCheckedInAppointments.
+            if (success) return await getCheckedInAppointmentsLoop(patientSerNum);
             else throw lastError;
         }
         else return [];
@@ -477,6 +478,41 @@ function getCheckedInAppointments(patientSerNum){
             .then(rows => resolve({Response:'success', Data: rows}))
             .catch(err => reject({Response: 'error', Reason: err}));
     })
+}
+
+/**
+ * @description This function is a TEMPORARY patch, and does not represent the right way of doing things.
+ *              Until the check-in race condition is properly fixed, this patch will help restore check-in service in prod.
+ *              Race condition: "await checkIntoOIE" resolves as a success BEFORE the check-in status is actually updated in OpalDB,
+ *              i.e. before the check-in is fully completed. This causes getCheckedInAppointments to return [],
+ *              even upon a successful checkin via the OIE, causing an error in the app.
+ *
+ *              This function checks the database once per second, until it detects that check-in was completed,
+ *              up to a maximum attempt limit. This should alleviate the race condition by forcing the listener
+ *              to wait a bit for the OIE call to fully complete. If check-in has not completed by the time limit,
+ *              then the last faulty result is returned. Once the race condition is fixed, this function should be deleted.
+ *
+ *              Patch requested by Yick Mo.
+ * @param patientSerNum The PatientSerNum of the patient being checked in.
+ */
+async function getCheckedInAppointmentsLoop(patientSerNum) {
+    // Source: https://stackoverflow.com/questions/14249506/how-can-i-wait-in-node-js-javascript-l-need-to-pause-for-a-period-of-time
+    const waitOneSecond = () => new Promise(resolve => setTimeout(resolve, 1000));
+    const maxNumberOfSeconds = 15;
+    logger.log('verbose', `Waiting for check-in status to be updated in OpalDB for PatientSerNum = ${patientSerNum}`);
+
+    let checkedInAppointments;
+    for (let i = 0; i < maxNumberOfSeconds; i++) {
+        await waitOneSecond();
+        checkedInAppointments = await getCheckedInAppointments(patientSerNum);
+        if (checkedInAppointments.Data && checkedInAppointments.Data.length !== 0) {
+            logger.log('verbose', `Check-in status update was detected in OpalDB for PatientSerNum = ${patientSerNum}, ${JSON.stringify(checkedInAppointments)}`);
+            return checkedInAppointments;
+        }
+        logger.log('verbose', `Check-in status has not yet been updated in OpalDB after ${i+1} ${i+1===1 ? 'second' : 'seconds'} for PatientSerNum = ${patientSerNum}`);
+    }
+    logger.log('verbose', `Check-in timeout of ${maxNumberOfSeconds} seconds reached for PatientSerNum = ${patientSerNum}, ${JSON.stringify(checkedInAppointments)}`);
+    return checkedInAppointments;
 }
 
 /**
