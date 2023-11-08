61263c1ca29d04c09e9bf291546fda6aee718873
Stacey Beard <staceybeard10@gmail.com>
2023-11-08 22:20:19 +0000

    fix: add request to get a single questionnaire stub
    Added onto the 'GetOneItem' request to be able to request a single questionnaire stub (same as those requested in 'QuestionnaireList'). This was used to fix a bug where a questionnaire could not be resumed from notifications, because some information was missing and was not being saved to the service.
    
    *More details*
    
    * Marked `QuestionnaireInOpalDBFromSerNum` as deprecated.
    * Modified the `GetOneItem` request to be able to process single items for modules (implemented in `listener/api/modules/`).
    
    See merge request opalmedapps/opal-listener#211

--- listener/api/modules/questionnaires/index.js
+++ listener/api/modules/questionnaires/index.js
@@ -1,4 +1,5 @@
 // Partial index of requests used by sqlInterface's requestMappings
 module.exports = {
-    'QuestionnaireList':  require('./request-handlers/questionnaire-list.request-handler.js'),
+    'QuestionnaireList': require('./request-handlers/questionnaire-list.request-handler.js'),
+    'QuestionnaireListSingle': require('./request-handlers/questionnaire-list-single.request-handler.js'),
 };
