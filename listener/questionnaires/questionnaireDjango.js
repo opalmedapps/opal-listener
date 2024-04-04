f325ded7911528898767e0401de5ee29f1841aaf
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2024-04-04 18:59:10 +0000

    feat: check caregiver permissions for answering questionnaires on behalf of the patient
    Closes QSCCD-1908
    
    See merge request opalmedapps/opal-listener!290

--- listener/questionnaires/questionnaireDjango.js
+++ listener/questionnaires/questionnaireDjango.js
@@ -41,23 +41,30 @@ class QuestionnaireDjango {
     }
 
     /**
-     * @desc Interprets a list of relationships between a caregiver and a given patient to determine if at least one of
-     *       them is a "self" relationship (meaning that the caregiver is the patient).
+     * @desc Interprets a list of relationships between a caregiver and a given patient to determine if they
+     *       represent a "self" relationship (meaning that the caregiver is the patient).
+     *       Note that in practice there should only be one such confirmed relationship.
      * @param {Object[]} relationships The list of relationships to check.
-     * @returns {boolean} True if at least one of the relationships has the role type "self".
+     * @returns {boolean} True if all of the confirmed relationships have the role type "self".
      */
     static caregiverIsSelf(relationships) {
-        return relationships.some(relationship => relationship.relationship_type.role_type === "SELF");
+        // Check only confirmed relationships
+        let confirmedRelationships = relationships.filter(patientRelationship => patientRelationship.status === 'CON');
+        return confirmedRelationships.every(relationship => relationship.relationship_type.role_type === "SELF");
     }
 
     /**
-     * @desc Interprets a list of relationships between a caregiver and a given patient to determine if at least one of
-     *       them allows the caregiver to respond to patient questionnaires.
-     * @param {Object[]} relationships The list of relationships to check.
+     * Check if given caregiver can answer patient's questionnaires.
+     *
+     * @param {string} userId The Firebase username of the user making the request.
+     * @param {number} patientSerNum The PatientSerNum of the care-receiver.
      * @returns {boolean} True if at least one of the relationships has "can_answer_questionnaire" enabled.
      */
-    static caregiverCanAnswerQuestionnaires(relationships) {
-        return relationships.some(relationship => relationship.relationship_type.can_answer_questionnaire === true);
+    static async caregiverCanAnswerQuestionnaire(userId, patientSerNum) {
+        let relationships = await this.getRelationshipsWithPatient(userId, patientSerNum);
+        // Check only confirmed relationships
+        let confirmedRelationships = relationships.filter(patientRelationship => patientRelationship.status === 'CON');
+        return confirmedRelationships.every(relationship => relationship.relationship_type.can_answer_questionnaire === true);
     }
 }
 
