acff5cf6def4b4d2f5ba055586fec8f71dd76d01
Matthias Schoettle <git@mattsch.com>
2025-01-24 18:47:54 +0000

    refactor: improve environment variable naming and don't require environment variables when the feature is disabled
    See merge request opalmedapps/opal-listener#405

--- listener/config-adaptor.js
+++ listener/config-adaptor.js
@@ -12,8 +12,8 @@ module.exports.DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
 module.exports.FIREBASE_ROOT_BRANCH = process.env.FIREBASE_ROOT_BRANCH;
 module.exports.MYSQL_USERNAME = process.env.MYSQL_USERNAME;
 module.exports.MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
-module.exports.MYSQL_DATABASE = process.env.MYSQL_DATABASE;
-module.exports.MYSQL_DATABASE_QUESTIONNAIRE = process.env.MYSQL_DATABASE_QUESTIONNAIRE;
+module.exports.MYSQL_DATABASE = "OpalDB";
+module.exports.MYSQL_DATABASE_QUESTIONNAIRE = "QuestionnaireDB";
 module.exports.MYSQL_DATABASE_HOST = process.env.MYSQL_DATABASE_HOST;
 module.exports.BACKEND_HOST = process.env.BACKEND_HOST;
 module.exports.BACKEND_LISTENER_AUTH_TOKEN = process.env.BACKEND_LISTENER_AUTH_TOKEN;
@@ -24,12 +24,10 @@ module.exports.DOCTOR_PATH = process.env.DOCTOR_PATH;
 module.exports.QUESTIONNAIRE_COMPLETED_URL = process.env.QUESTIONNAIRE_COMPLETED_URL;
 module.exports.DEFAULT_LAB_EDUCATIONAL_URL_EN = process.env.DEFAULT_LAB_EDUCATIONAL_URL_EN;
 module.exports.DEFAULT_LAB_EDUCATIONAL_URL_FR = process.env.DEFAULT_LAB_EDUCATIONAL_URL_FR;
-module.exports.LATEST_STABLE_VERSION = process.env.LATEST_STABLE_VERSION;
-// OPAL EMAIL used in `mailer.js`
-module.exports.OPAL_EMAIL=process.env.OPAL_EMAIL;
-// SMTP IP and PORT used in `mailer.js`
-module.exports.SMTP_SERVER_IP=process.env.SMTP_SERVER_IP;
-module.exports.SMTP_SERVER_PORT=process.env.SMTP_SERVER_PORT;
+module.exports.LATEST_STABLE_VERSION = process.env.LATEST_STABLE_VERSION ?? "0.0.1";
+module.exports.FEEDBACK_EMAIL=process.env.FEEDBACK_EMAIL;
+module.exports.FEEDBACK_EMAIL_HOST=process.env.FEEDBACK_EMAIL_HOST;
+module.exports.FEEDBACK_EMAIL_PORT=process.env.FEEDBACK_EMAIL_PORT;
 // Checkins
 module.exports.ORMS_ENABLED = process.env.ORMS_ENABLED === '1';
 module.exports.ORMS_CHECKIN_URL = process.env.ORMS_CHECKIN_URL;
