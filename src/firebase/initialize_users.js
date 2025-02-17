a6ec83303f1932e5e9ee7470795b055b992e7df3
Matthias Schoettle <git@mattsch.com>
2025-02-17 16:46:37 +0000

    chore: allow to keep non-test users when initializing Firebase users
    See merge request opalmedapps/opal-listener!415

--- src/firebase/initialize_users.js
+++ src/firebase/initialize_users.js
@@ -13,6 +13,55 @@ const { getAuth } = require('firebase-admin/auth');
 const { FIREBASE_CONFIG } = require('../environment');
 const { Firebase } = require('./firebase');
 
+const USER_RECORDS = [
+    {
+        email: 'marge@opalmedapps.ca',
+        password: '12345Opal!!',
+        uid: 'QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2',
+    },
+    {
+        email: 'homer@opalmedapps.ca',
+        password: '12345Opal!!',
+        uid: 'PyKlcbRpMLVm8lVnuopFnFOHO4B3',
+    },
+    {
+        email: 'bart@opalmedapps.ca',
+        password: '12345Opal!!',
+        uid: 'SipDLZCcOyTYj7O3C8HnWLalb4G3',
+    },
+    {
+        email: 'mona@opalmedapps.ca',
+        password: '12345Opal!!',
+        uid: '61DXBRwLCmPxlaUoX6M1MP9DiEl1',
+    },
+    {
+        email: 'fred@opalmedapps.ca',
+        password: '12345Opal!!',
+        uid: 'ZYHAjhNy6hhr4tOW8nFaVEeKngt1',
+    },
+    {
+        email: 'laurie@opalmedapps.ca',
+        password: '12345Opal!!',
+        uid: 'a51fba18-3810-4808-9238-4d0e487785c8',
+    },
+    {
+        email: 'rory@opalmedapps.ca',
+        password: '12345Opal!!',
+        uid: 'mouj1pqpXrYCl994oSm5wtJT3In2',
+    },
+    {
+        email: 'bobbyjones@demo.opalmedapps.ca',
+        password: '12345Opal!!',
+        uid: 'hIMnEXkedPMxYnXeqNXzphklu4V2',
+    },
+    // A user registered at another institution
+    {
+        email: 'apptest+ned@opalmedapps.ca',
+        password: '12345Opal!!',
+        uid: '9wajsHGHaMUdDIcepyUJkm4O1pG3',
+    },
+];
+
 /**
  * @description Deletes all Firebase users.
  * @returns {Promise<void>} Rejects with an error if listing or deleting Firebase users fails.
@@ -32,52 +81,25 @@ async function deleteAllUsers() {
  * @description Creates a defined list of new Firebase users.
  * @returns {Promise<void>}
  */
-async function createUsers() {
-    const userRecords = [
-        {
-            email: 'marge@opalmedapps.ca',
-            password: '12345Opal!!',
-            uid: 'QXmz5ANVN3Qp9ktMlqm2tJ2YYBz2',
-        },
-        {
-            email: 'homer@opalmedapps.ca',
-            password: '12345Opal!!',
-            uid: 'PyKlcbRpMLVm8lVnuopFnFOHO4B3',
-        },
-        {
-            email: 'bart@opalmedapps.ca',
-            password: '12345Opal!!',
-            uid: 'SipDLZCcOyTYj7O3C8HnWLalb4G3',
-        },
-        {
-            email: 'mona@opalmedapps.ca',
-            password: '12345Opal!!',
-            uid: '61DXBRwLCmPxlaUoX6M1MP9DiEl1',
-        },
-        {
-            email: 'fred@opalmedapps.ca',
-            password: '12345Opal!!',
-            uid: 'ZYHAjhNy6hhr4tOW8nFaVEeKngt1',
-        },
-        {
-            email: 'laurie@opalmedapps.ca',
-            password: '12345Opal!!',
-            uid: 'a51fba18-3810-4808-9238-4d0e487785c8',
-        },
-        // A user registered at another institution
-        {
-            email: 'apptest+ned@opalmedapps.ca',
-            password: '12345Opal!!',
-            uid: '9wajsHGHaMUdDIcepyUJkm4O1pG3',
-        },
-        {
-            email: 'rory@opalmedapps.ca',
-            password: '12345Opal!!',
-            uid: 'mouj1pqpXrYCl994oSm5wtJT3In2',
-        },
-    ];
+async function deleteUsers() {
+    for (const user of USER_RECORDS) {
+        try {
+            // eslint-disable-next-line no-await-in-loop
+            await getAuth().deleteUser(user.uid);
+            console.log('Successfully deleted user:', user.email);
+        }
+        catch (error) {
+            console.log('Error deleting user:', user.email, error);
+        }
+    }
+}
 
-    for (const user of userRecords) {
+/**
+ * @description Creates a defined list of new Firebase users.
+ * @returns {Promise<void>}
+ */
+async function createUsers() {
+    for (const user of USER_RECORDS) {
         try {
             // eslint-disable-next-line no-await-in-loop
             const userRecord = await getAuth().createUser(user);
@@ -91,15 +113,28 @@ async function createUsers() {
 
 /**
  * @description Delete all Firebase users and create a pre-defined list of new users.
+ * @param {boolean} [shouldDeleteAllUsers] Whether to delete all users before creating new ones,
+ * if false, only the pre-defined users will be deleted.
  * @returns {Promise<void>} Rejects with an error if any error occurs
  */
-async function run() {
+async function run(shouldDeleteAllUsers) {
     const firebase = new Firebase(FIREBASE_CONFIG);
     await firebase.init();
 
-    await deleteAllUsers();
+    if (shouldDeleteAllUsers) {
+        await deleteAllUsers();
+    }
+    else {
+        await deleteUsers();
+    }
     await createUsers();
     process.exit(0);
 }
 
-run();
+let shouldDeleteAllUsers = false;
+
+if (process.argv.includes('--delete-all-users')) {
+    shouldDeleteAllUsers = true;
+}
+
+run(shouldDeleteAllUsers);
