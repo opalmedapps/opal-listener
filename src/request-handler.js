c80383525673ab428bcb51039c72b1966d5ea258
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-02-26 20:50:42 +0000

    ADD: Handling of new vs legacy request ADD: Just-clone library to deep clone request object to identify new from old requests FIX: Code according to eslint
    (cherry picked from commit e7a188d735d1c88e319cc55fe09b3b626dd15fa6)
    
    Co-authored-by: David Gagne <dgagne@Opal-MacBook-Air.local>

--- src/request-handler.js
+++ src/request-handler.js
@@ -1,89 +1,95 @@
 /**
  * @file Listen and handle request from firebase
- * @author David Gagne, Donald Duck
+ * @author David Gagne
  */
-const logger = require('../listener/logs/logger');
+const JC = require('just-clone');
+const legacyLogger = require('../listener/logs/logger');
 const legacyServer = require('../listener/legacy-server');
 const legacyProcessApi = require('../listener/api/processApiRequest');
-const legacyMainRequestApi = require('../listener/api/main.js');
+const legacyMainRequestApi = require('../listener/api/main');
+const legacyRequestValidator = require('../listener/api/request/request-validator');
 
 class RequestHandler {
     /**
      * @description Reference to the firebase database connection
      */
     #databaseRef;
+
     /**
      * @description Create an instance of the RequestHandler class and set connection to firebase
-     * @param  {Class} firebase - Firebase manager class
+     * @param {object} firebase - Firebase manager class
      */
     constructor(firebase) {
-        this.#setFirebaseConnection(firebase);
-    }
-    /**
-     * Create the firebase database reference.
-     * @param  {Class} firebase - Firebase manager class
-     */
-    #setFirebaseConnection(firebase) {
         this.#databaseRef = firebase.database.ref(firebase.root);
-        this.#databaseRef.set(null).catch(function (error) {
-            logger.log('error', 'Cannot reset firebase', error);
-        });
     }
+
     /**
-     * Getter for the database reference to pass to legacy-server to keep it working for now
+     * @description Getter for the database reference to pass to legacy-server to keep it working for now
+     * wihtout having 2 database connections open
+     * @returns {object} Database reference used in legacy-listener
      */
     get databaseRef() {
         return this.#databaseRef;
     }
+
     /**
      * @description Listen to firebase request, pass it to the correct handler and upload the response to firebase.
-     * @param  {Object} requestType - Resquest data from Opal firebase
+     * @param  {object} requestType - Resquest data from Opal firebase
      */
     listenToRequest(requestType) {
         this.#databaseRef.child(requestType).off();
-        this.#databaseRef.child(requestType).on('child_added', async (snapshot) => {
-            const isSecurityRequest = legacyProcessApi.securityAPI.hasOwnProperty(snapshot.val().Request);
+        this.#databaseRef.child(requestType).on('child_added', async snapshot => {
+            const isSecurityRequest = Object.prototype.hasOwnProperty.call(
+                legacyProcessApi.securityAPI,
+                snapshot.val().Request,
+            );
             try {
-                const processedRequest = isSecurityRequest ? await this.processSecurityRequests(snapshot) : await this.processRequests(snapshot);
+                const processedRequest = isSecurityRequest
+                    ? await this.processSecurityRequests(snapshot)
+                    : await RequestHandler.processRequests(snapshot);
                 await legacyServer.uploadToFirebase(processedRequest, requestType);
-            } catch (error) {
-                logger.log('error', 'CANNOT PROCESS REQUEST', error);
+            }
+            catch (error) {
+                legacyLogger.log('error', 'CANNOT PROCESS REQUEST', error);
             }
         });
     }
+
     /**
      * @description Handle security requests
-     * @param {Object} snapshot - Response data snapshit from firebase
-     * @returns Data from the security API
+     * @param {object} snapshot - Response data snapshot from firebase
+     * @returns {object} Data from the security API
      */
-    processSecurityRequests(snapshot) {
-        return new Promise(async (resolve, reject) => {
-            const requestKey = snapshot.key;
-            const requestObject = snapshot.val();
-            try {
-                const response = await legacyProcessApi.securityAPI[requestObject.Request](requestKey, requestObject);
-                resolve(response);
-            } catch (error) {
-                reject(error);
-            }
-        });
+    static async processSecurityRequests(snapshot) {
+        const requestKey = snapshot.key;
+        const requestObject = snapshot.val();
+        try {
+            const response = await legacyProcessApi.securityAPI[requestObject.Request](requestKey, requestObject);
+            return response;
+        }
+        catch (error) {
+            return error;
+        }
     }
+
     /**
-     * @description Handle regular requests
-     * @param {Object} snapshot - Response data snapshit from firebase
-     * @returns Data from the request API
+     * @description Handle regular requests. If requestType is 'api', send to new pipeline else send to legacy listener
+     * @param {object} snapshot - Response data snapshot from firebase
+     * @returns {object} Data from the request API
      */
-    processRequests(snapshot) {
-        return new Promise(async (resolve, reject) => {
-            const requestKey = snapshot.key;
-            const requestObject = snapshot.val();
-            try {
-                const response = await legacyMainRequestApi.apiRequestFormatter(requestKey, requestObject);
-                resolve(response);
-            } catch (error) {
-                reject(error);
-            }
-        });
+    static async processRequests(snapshot) {
+        const requestKey = snapshot.key;
+        const requestObject = snapshot.val();
+        const requestType = await legacyRequestValidator.validate(requestKey, JC(requestObject));
+        try {
+            const response = (requestType.type === 'api')
+                ? console.log('send to new pipeline') // TO DO setup new pipeline
+                : await legacyMainRequestApi.apiRequestFormatter(requestKey, requestObject);
+            return response;
+        }
+        catch (error) {
+            return error;
+        }
     }
 }
 
