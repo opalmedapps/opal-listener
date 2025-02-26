b88b65d7eb131267f151a88b19ee5f4d409c870d
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-02-26 21:41:20 +0000

    fix: remove eslint-disable
    (cherry picked from commit f75e5e1c3c2816f9bc8b907701af51cb4a110758)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- listener/mailer/mailer.js
+++ listener/mailer/mailer.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line max-len
 // SPDX-FileCopyrightText: Copyright 2016 Opal Health Informatics Group at the Research Institute of the McGill University Health Centre <john.kildea@mcgill.ca>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
