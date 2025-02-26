b7e6f5219178d38d4e29e6c8f433dec235b87570
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-02-26 20:49:34 +0000

    WIP: Add custom messages format to logger.
    (cherry picked from commit f6a414b55d366cfbb4fa061cb01c777c0c42fa34)
    
    Co-authored-by: David Gagne <david.gagne@muhc.mcgill.ca>

--- listener/logs/logger.js
+++ listener/logs/logger.js
@@ -13,36 +13,33 @@ const {createLogger, format, transports} = require('winston');
 // Choose log level according to environement.
 const LoggerLevel = process.env.NODE_ENV === 'production' ? 'info' : 'debug';
 
+// Set custom format for login.
+const opalLogFormat = format.printf((info) => {
+    const { level, message, timestamp, data } = info;
+    return `${timestamp} - ${level.toUpperCase()}: ${message} ${(data) ? `- DATA: ${data}` : ``}`;
+});
+
 // Initialize winston logger
 const WinstonLogger = createLogger({
     // Set level log level
     level: LoggerLevel,
     // Set log format output in the log file
     format: format.combine(
-        format.timestamp(),
-        format.json()
+        format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+        format.json(),
+        opalLogFormat
     ),
     transports: [
         // Log level info and above in the log file.
-        new transports.File({ filename:'./logs/opal-info.log', level: 'info'})
+        new transports.File({ filename:'./logs/opal-info.log', level: 'info'}),
+        new transports.Console({level: 'debug'})
     ],
     exceptionHandlers: [
-        // Log uncaught exceptions to a different file.
-        new transports.File({ filename: './logs/opal-uncaughtExceptions.log'})
+        // Log uncaught exceptions to a different file and console.
+        new transports.File({ filename: './logs/opal-uncaughtExceptions.log'}),
+        new transports.Console({level: 'debug'})
     ]
 });
-
-// Init debug transport that output to the console only if we are not in production.
-if (process.env.NODE_ENV !== 'production') {
-    WinstonLogger.add(new transports.Console({
-        level: 'debug',
-        format: format.combine(
-            format.colorize(),
-            format.simple(),
-            format.timestamp()
-        ),
-    }));
-}
 
 WinstonLogger.log('info', `Initialized Winston with level: ${LoggerLevel}`);
 
