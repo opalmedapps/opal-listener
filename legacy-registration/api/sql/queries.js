aaea76e10509095d69571bcb07a0a13da9fce91a
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-02-26 20:55:36 +0000

    Rename getLabResultHistory parameter
    (cherry picked from commit e8adb3941d578838a1ff4ab3ab287cf43112299e)
    
    Co-authored-by: Limin <llmgd@hotmail.com>

--- legacy-registration/api/sql/queries.js
+++ legacy-registration/api/sql/queries.js
@@ -42,10 +42,6 @@ exports.insertPatientHospitalIdentifier = function () {
     return "SELECT reg_insertPatientHospitalIdentifier(?,?,?) AS Result;";
 };
 
-exports.validatePatient = function () {
-    return "SELECT validatePatient(?) AS Result;";
-};
-
 exports.getSiteAndMrn = function() {
     return "SELECT PHI.MRN AS Mrn, PHI.Hospital_Identifier_Type_Code AS Site FROM Patient_Hospital_Identifier AS PHI INNER JOIN Patient AS P ON PHI.PatientSerNum = P.PatientSerNum AND P.SSN = ?;";
 };
