06588ac98394ce31d12158d57e4cdf2a8095fb5a
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-02-26 21:05:51 +0000

    feat deps refactor the code
    (cherry picked from commit e5c1846783a8545930908907b802bec7e9cca59c)
    
    Co-authored-by: Limin <llmgd@hotmail.com>

--- legacy-registration/api/firebase/firebaseFunctions.js
+++ legacy-registration/api/firebase/firebaseFunctions.js
@@ -1,10 +1,6 @@
 /**  Library Imports **/
 
 const admin = require("firebase-admin");
-const logger = require('../../logs/logger.js');
-const Q = require('q');
-const config = require('../../config-adaptor');
-
 
 // Function to create firebase account
 exports.createFirebaseAccount = async function (userEmail, userPassword) {
@@ -24,31 +20,3 @@ exports.getFirebaseAccountByEmail = async function (userEmail) {
 exports.deleteFirebaseAccount = async function (uid) {
     await admin.auth().deleteUser(uid)
 };
-//const serviceAccount = require(config.FIREBASE_ADMIN_KEY);
-
-//admin.initializeApp({
-//    credential: admin.credential.cert(serviceAccount),
-//    databaseURL: config.DATABASE_URL
-//});
-
-//// Enable firebase logging
-//admin.database.enableLogging(false);
-
-//// Get reference to correct data element
-//const db = admin.database();
-//const ref = db.ref(config.firebaseBranch.parentBranch);
-
-
-//admin.auth().createUser({
-//    email: 'test@test.com',
-//    password: 'TEst'
-//})
-//    .then(function (userRecord) {
-//        // Firebase account has been created and it returns the firebase unique ID
-//        logger.log('debug', 'Successfully created new firebase user account and this is the unique Id: ' + userRecord.uid);
-//        r.resolve(userRecord.uid);
-//    })
-//    .catch(function (error) {
-//        logger.log('Error', 'Error while creating new user account in firebase' + error.code);
-//        r.resolve(error.code);
-//    });
