f144d8fe0b5414d1424bfd3da3923fcd0cb1b15f
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-02-26 20:54:50 +0000

    Adjustments following code review
    (cherry picked from commit 944a47ee5bdd64c826936ac1641dbf59ff191549)
    
    Co-authored-by: David Gagne <dgagne@Opal-MacBook-Air.local>

--- src/core/request-handler.js
+++ src/core/request-handler.js
@@ -13,19 +13,9 @@ class RequestHandler {
     #databaseRef;
 
     /**
-     * @description Key to identify Firebase requests.
-     */
-    #firebaseRequestKey;
-
-    /**
      * Encryption utility classes used throughout the request handling process.
      */
     #encryptionUtils;
-
-    /**
-     * User ID from the user who made the request.
-     */
-    #userId;
 
     /**
      * @description Create an instance of the RequestHandler class and set connection to firebase
@@ -36,17 +26,17 @@ class RequestHandler {
     }
 
     /**
-     * @description Listen to firebase request, pass it to the correct handler and upload the response to firebase.
+     * @description Listen to firebase request, and upload the response to firebase.
      * @param  {object} requestType - Resquest data from Opal firebase
      */
     listenToRequests(requestType) {
-        legacyLogger.log('debug', 'API: Strting request listener');
+        legacyLogger.log('debug', 'API: Starting request listener');
         this.#databaseRef.child(requestType).off();
         this.#databaseRef.child(requestType).on('child_added', async snapshot => {
             try {
                 const response = await this.#processRequest(snapshot);
-                await this.sendResponse(response);
-                await this.clearRequest(requestType);
+                await this.sendResponse(response, snapshot.key, snapshot.val().UserID);
+                await this.clearRequest(requestType, snapshot.key);
             }
             catch (error) {
                 legacyLogger.log('error', 'API: CANNOT PROCESS REQUEST', error);
@@ -55,22 +45,25 @@ class RequestHandler {
     }
 
     /**
-     * @description Remove request from Firebase once it is process.
+     * @description Remove request from Firebase once it is processed.
      * @param {string} requestType Type of request sent to Firebase.
+     * @param {string} firebaseRequestKey Firebase request unique identifier
      */
-    async clearRequest(requestType) {
+    async clearRequest(requestType, firebaseRequestKey) {
         legacyLogger.log('debug', 'API: Clearing request from Firebase');
-        await this.#databaseRef.child(requestType).child(this.#firebaseRequestKey).set(null);
+        await this.#databaseRef.child(requestType).child(firebaseRequestKey).set(null);
     }
 
     /**
      * @description Send response to Firebase after being processed.
      * @param {object} response Process unencrypted response.
+     * @param {string} firebaseRequestKey Firebase request unique identifier
+     * @param {string} userId User id making the request.
      */
-    async sendResponse(response) {
+    async sendResponse(response, firebaseRequestKey, userId) {
         legacyLogger.log('debug', 'API: Sending response to Firebase');
         const encryptedResponse = await this.#encryptionUtils.encryptResponse(response);
-        const path = `users/${this.#userId}/${this.#firebaseRequestKey}`;
+        const path = `users/${userId}/${firebaseRequestKey}`;
         await this.#databaseRef.child(path).set(encryptedResponse);
     }
 
@@ -81,10 +74,8 @@ class RequestHandler {
      */
     async #processRequest(snapshot) {
         legacyLogger.log('debug', 'API: Processing API request');
-        this.#firebaseRequestKey = snapshot.key;
         this.#encryptionUtils = new EncryptionUtilities(snapshot.val());
         const decryptedRequest = await this.#encryptionUtils.decryptRequest();
-        this.#userId = decryptedRequest.UserID;
         return ApiRequest.makeRequest(decryptedRequest);
     }
 }
