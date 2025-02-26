654ba5a1156d25df8437a050d4fb171739865349
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-02-26 21:38:24 +0000

    remove filter
    (cherry picked from commit d94c714ecd7e71fe710a27157b18c1306819082e)
    
    Co-authored-by: Douzeyu <dou3180@hotmail.com>

--- listener/sql/queries.js
+++ listener/sql/queries.js
@@ -108,8 +108,6 @@ function patientAppointmentTableFields(selectOne) {
                 LEFT JOIN EducationalMaterialControl emc ON emc.EducationalMaterialControlSerNum = A.EducationalMaterialControlSerNum
             WHERE
                 Appt.PatientSerNum = ?
-                AND Appt.State = 'Active'
-                AND Appt.Status <> 'Deleted'
                 ${selectOne ? 'AND Appt.AppointmentSerNum = ?' : ''}
                 ${!selectOne ? 'AND (Appt.LastUpdated > ? OR A.LastUpdated > ? OR AE.LastUpdated > ? OR HM.LastUpdated > ?)' : ''}
             ORDER BY Appt.AppointmentSerNum, ScheduledStartTime;
