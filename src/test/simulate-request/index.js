59a4d210be23895d337b899adc62e28f4d6e9735
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-02-26 20:51:46 +0000

    Create simulate request class to help testing
    (cherry picked from commit ce9ed65c553c10c6e73821bf552c3e692a0f97c5)
    
    Co-authored-by: David Gagne <dgagne@Opal-MacBook-Air.local>

---
+++ src/test/simulate-request/index.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-new */
+/**
+ * @file Upload a mock request to firebase to be picked up by the listener
+ * @author David Gagne
+ */
+const path = require('path');
+const fs = require('fs');
+const config = require('../../config/config.json');
+const { Firebase } = require('../../firebase/firebase');
+const requestData = require('./mock-requests');
+const legacyLogger = require('../../../listener/logs/logger');
+
+class SimulateRequest {
+    /**
+     * @description App configuration needed to connect to firebase
+     */
+    #config;
+
+    /**
+     * @description Mocked request data
+     */
+    #requestData;
+
+    constructor() {
+        this.#config = config;
+        this.#requestData = requestData;
+        this.makeRequest();
+    }
+
+    /**
+     * @description Get the firebase admin keep from it's folder and trigger forebase initialization.
+     */
+    makeRequest() {
+        const pathName = path.join(__dirname, '../../config/firebase');
+        fs.readdir(pathName, (err, files) => {
+            if (err) throw new Error('Can\'t load firebase admin key');
+            this.#config.FIREBASE.ADMIN_KEY_PATH = `${pathName}/${files[0]}`;
+            this.initFirebase();
+        });
+    }
+
+    /**
+     * @description Init Firebase connection and trigger upload.
+     */
+    async initFirebase() {
+        const firebase = new Firebase(this.#config.FIREBASE);
+        await firebase.init();
+        Firebase.enableLogging(this.#config.FIREBASE.ENABLE_LOGGING);
+        this.#requestData.Timestamp = Firebase.getDatabaseTimeStamp;
+        this.uploadToFirebase(firebase.getDataBaseRef);
+    }
+
+    /**
+     * @description Upload the mock request to firebase db.
+     * @param {object} databaseRef - Firebase database connection reference.
+     */
+    uploadToFirebase(databaseRef) {
+        databaseRef.child('requests').push(this.#requestData).then(response => {
+            legacyLogger.log('debug', `Firebase request push succes with key: ${response.key}`);
+            process.exit();
+        }).catch(error => {
+            throw new Error(error);
+        });
+    }
+}
+
+new SimulateRequest();
---
+++ src/test/simulate-request/mock-requests.js
@@ -0,0 +1,20 @@
+/* eslint-disable max-len */
+/**
+ * @description Encripted mock request
+ */
+const requestData = {
+    AppVersion: '100.100.100',
+    Request: 'HqTzYD6FCB7ze7gVZSoCJ/41P5VxT2SG2W0iPYTginfrKRp5HljSaExInpC0',
+    DeviceId: 'a312e0e60b00ac0d30130540-3b1370f0a50-4a50e8020-3fc210710880-4b1e305601a0e808903f0a00b80a80120460',
+    Token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImM2NzNkM2M5NDdhZWIxOGI2NGU1OGUzZWRlMzI1NWZiZjU3NTI4NWIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vb3BhbC01ZGRkMiIsImF1ZCI6Im9wYWwtNWRkZDIiLCJhdXRoX3RpbWUiOjE2NTA5MTIyNDcsInVzZXJfaWQiOiJKVVl4SmFkUXVoaE9rQzFUZnJBcUQ0Y3JoaTczIiwic3ViIjoiSlVZeEphZFF1aGhPa0MxVGZyQXFENGNyaGk3MyIsImlhdCI6MTY1MDkxMjI0NywiZXhwIjoxNjUwOTE1ODQ3LCJlbWFpbCI6Im11aGMuYXBwLm1vYmlsZUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsibXVoYy5hcHAubW9iaWxlQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.HH1P3IIuVMu96RkpcyKYCF_OGjT2S1w0LUk-ntQbSsp3gzP3bmlTPHucTe84jXmk-p9qXiKkRISE0Kcs1MuogHi-ohCrZr7Wm0xEARpqDHKXTBZY9lSMKC9s7jjkbDAuAmiuPWT4vNipe9KI-xq-ZLXv_SjFd3MCYg1ROfegBxsyfPegxSR-A2l8qj82j--bvx_jgbe6sVusW5ipgU1SMxBIXJ6IVlhn7zp3VJWH1AizXqgPJtFDblzFP719FirgOxH98OT7deBTRV_KU0JAGhlLJOJRyMyOKs6gtUlIyH5IrR4PzJsTOjAjL2lNkWmcCFOPYkF15f_B1u2KER-Yrg',
+    UserID: 'JUYxJadQuhhOkC1TfrAqD4crhi73',
+    Parameters: {
+        Fields: [
+            'Uc46RnDg2BlkAn94bVjiMhT+VzhXaYmqsN4SnuCB39XMRWIpFzy6OoJ1xw08Z0s=',
+        ],
+    },
+    Timestamp: 'admin.database.ServerValue.TIMESTAMP',
+    UserEmail: 'muhc.app.mobile@gmail.com',
+};
+
+module.exports = requestData;
