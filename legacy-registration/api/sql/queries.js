ca23cbba27d3ad0704a37696c5abe150be780a20
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-02-26 21:13:52 +0000

    remove delete expired branches cron
    (cherry picked from commit 09430be72d306cdf594390ccfc6527e648f0300b)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

--- legacy-registration/api/sql/queries.js
+++ legacy-registration/api/sql/queries.js
@@ -44,43 +44,9 @@ exports.getSiteAndMrn = function() {
 
 exports.getPatient = function() {
     return `
-        SELECT patient.FirstName, patient.LastName, patient.TelNum, patient.BlockedStatus, \`phi\`.MRN, \`hit\`.Description_EN AS hospital_name_EN, \`hit\`.Description_FR AS hospital_name_FR 
-        FROM Patient patient 
-        JOIN Patient_Hospital_Identifier \`phi\` ON \`phi\`.PatientSerNum = patient.PatientSerNum 
+        SELECT patient.FirstName, patient.LastName, patient.TelNum, patient.BlockedStatus, \`phi\`.MRN, \`hit\`.Description_EN AS hospital_name_EN, \`hit\`.Description_FR AS hospital_name_FR
+        FROM Patient patient
+        JOIN Patient_Hospital_Identifier \`phi\` ON \`phi\`.PatientSerNum = patient.PatientSerNum
         JOIN Hospital_Identifier_Type \`hit\` ON \`hit\`.code = \`phi\`.Hospital_Identifier_Type_Code WHERE patient.SSN = ?;
     `;
 };
-
-/**
- * @desc Query that marks all expired registration branches for deletion by setting their DeleteBranch flag to 1.
- * @returns {string} The query.
- */
-exports.flagFirebaseBranchesForDeletion = () => {
-    return `UPDATE registrationcode
-            SET DeleteBranch = 1
-            WHERE Status = 'Expired'
-              AND DeleteBranch = 0
-            ;
-    `;
-}
-
-/**
- * @desc Query that returns all firebase branch names marked for deletion (with DeleteBranch = 1).
- * @returns {string} The query.
- */
-exports.getFirebaseBranchesToDelete = () => {
-    return `SELECT FirebaseBranch
-            FROM registrationcode
-            WHERE DeleteBranch = 1;
-    `;
-}
-
-/**
- * @desc Query that marks a set of firebase branches as deleted (by setting DeleteBranch = 2).
- * @returns {string} The query.
- */
-exports.markFirebaseBranchesAsDeleted = () => {
-    return `UPDATE registrationcode
-            SET DeleteBranch = 2
-            WHERE FirebaseBranch IN ?;`
-}
