4cbe4742192eab9f3be70e59b8181a9eba9deb2d
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-02-26 21:05:47 +0000

    feat deps refactor the apis
    (cherry picked from commit 0f242a1bef0e82a8b8c8c6d2c4d004b75518a7c6)
    
    Co-authored-by: Limin <llmgd@hotmail.com>

--- legacy-registration/api/firebase/firebaseFunctions.js
+++ legacy-registration/api/firebase/firebaseFunctions.js
@@ -7,59 +7,22 @@ const config = require('../../config-adaptor');
 
 
 // Function to create firebase account
-exports.createFirebaseAccount = function (userEmail, userPassword) {
-	let r = Q.defer();
-
-    admin.auth().createUser({
+exports.createFirebaseAccount = async function (userEmail, userPassword) {
+    const response = await admin.auth().createUser({
         email: userEmail,
         password: userPassword
-    })
-    .then(function (userRecord) {
-        // Firebase account has been created and it returns the firebase unique ID
-        logger.log('debug','Successfully created new firebase user account and this is the unique Id: ' + userRecord.uid);
-        r.resolve(userRecord.uid);
-    })
-    .catch(function (error) {
-        logger.log('error', 'Error while creating new user account in firebase: ' + error.code);
-        r.reject(error.errorInfo);
     });
-
-    return r.promise;
+    return response?.uid;
 };
 
 // Function to create firebase account
-exports.getFirebaseAccountByEmail = function (userEmail) {
-    let r = Q.defer();
-
-    admin.auth().getUserByEmail(userEmail)
-        .then(function (userRecord) {
-            // Firebase account has been created and it returns the firebase unique ID
-            logger.log('debug','Successfully getting firebase user account and this is the unique Id: ' + userRecord.uid);
-            r.resolve(userRecord.uid);
-        })
-        .catch(function (error) {
-            logger.log('error', 'Error while getting a user account in firebase: ' + error.code);
-            r.reject(error.errorInfo);
-        });
-
-    return r.promise;
+exports.getFirebaseAccountByEmail = async function (userEmail) {
+    const response = await admin.auth().getUserByEmail(userEmail)
+    return response?.uid;
 };
 
-exports.deleteFirebaseAccount = function (uid) {
-    let r = Q.defer();
-
-    admin.auth().deleteUser(uid)
-        .then(function () {
-            logger.log('debug', 'Successfully delete the firebase account using passed uniqueID: ' + uid);
-            r.resolve();
-        })
-        .catch(function (error) {
-            logger.log('Error', 'Error while deleting user account in firebase' + error);
-            r.reject();
-
-        });
-    return r.promise;
-
+exports.deleteFirebaseAccount = async function (uid) {
+    await admin.auth().deleteUser(uid)
 };
 //const serviceAccount = require(config.FIREBASE_ADMIN_KEY);
 
