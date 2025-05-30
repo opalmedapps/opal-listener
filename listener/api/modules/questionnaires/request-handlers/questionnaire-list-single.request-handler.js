775f5de3599b4c26a0367545528365d3f70e1dbc
Stacey Beard <staceybeard10@gmail.com>
2025-05-30 10:08:24 -0400

    chore: temporarily comment-out imports

--- listener/api/modules/questionnaires/request-handlers/questionnaire-list-single.request-handler.js
+++ listener/api/modules/questionnaires/request-handlers/questionnaire-list-single.request-handler.js
@@ -7,7 +7,7 @@ import logger from '../../../../logs/logger.js';
 import { param } from 'express-validator';
 import questionnaireOpalDB from '../../../../questionnaires/questionnaireOpalDB.js';
 import questionnaireQuestionnaireDB from '../../../../questionnaires/questionnaireQuestionnaireDB.js';
-import questionnaireConfig from '../../../../questionnaires/questionnaireConfig.json';
+// import questionnaireConfig from '../../../../questionnaires/questionnaireConfig.json';
 import utility from '../../../../utility/utility.js';
 import ValidationError from '../../../errors/validation-error.js';
 
