7f36c09b4663e1504e55c65ba945b7785b6137e9
Stacey Beard <staceybeard10@gmail.com>
2026-01-08 16:44:36 -0500

    fix: remove deprecated 1.12.2 compatibility for updating security questions

--- listener/api/modules/patient/security-questions/request-handlers/get-security-question-answer-list.request-handler.js
+++ listener/api/modules/patient/security-questions/request-handlers/get-security-question-answer-list.request-handler.js
@@ -5,7 +5,6 @@
 import ApiRequestHandler from '../../../../api-request-handler.js';
 import Patient from '../../patient.js';
 import SecurityDjango from '../../../../../security/securityDjango.js';
-import Version from '../../../../../../src/utility/version.js';
 
 class GetSecurityQuestionAnswerListRequestHandler extends ApiRequestHandler{
     /**
@@ -16,63 +15,19 @@ class GetSecurityQuestionAnswerListRequestHandler extends ApiRequestHandler{
     static async handleRequest(requestObject){
         const patient = await Patient.getPatientByUsername(requestObject.meta.UserID);
 
-        let activeSecurityQuestions;
-        let securityQuestionList;
-        let activeQuestionForLowerVersion = [];
-        let securityQuestionForLowerVersion = [];
-        let questionIdObj = {}
-
-        securityQuestionList = await SecurityDjango.getSecurityQuestionList(requestObject.meta.UserID);
+        let securityQuestionList = await SecurityDjango.getSecurityQuestionList(requestObject.meta.UserID);
         if (securityQuestionList.length === 0) throw "API call returned a empty list of questions for the current user";
 
-        activeSecurityQuestions = await SecurityDjango.getActiveSecurityQuestions(requestObject.meta.UserID);
+        let activeSecurityQuestions = await SecurityDjango.getActiveSecurityQuestions(requestObject.meta.UserID);
         if (activeSecurityQuestions.length === 0) throw "API call returned a empty list of all the active questions";
 
-        // Getting security question list format read by the app has changed after 1.12.2
-        if (Version.versionLessOrEqual(requestObject.meta.AppVersion, Version.version_1_12_2)) {
-            activeSecurityQuestions.forEach(questionObj => {
-                activeQuestionForLowerVersion.push(
-                    {
-                        "SecurityQuestionSerNum": questionObj.id,
-                        "QuestionText_EN": questionObj.title_en,
-                        "QuestionText_FR": questionObj.title_fr,
-                        "Active": 1
-                    }
-                );
-                questionIdObj[questionObj.title_en] = questionObj.id;
-                questionIdObj[questionObj.title_fr] = questionObj.id;
-            });
-
-            securityQuestionList.forEach(securityQuestionObj => {
-                let questionId = -1;
-                if (questionIdObj[securityQuestionObj.question] !== "undefined") questionId = questionIdObj[securityQuestionObj.question];
-                securityQuestionForLowerVersion.push(
-                    {
-                        "SecurityAnswerSerNum": securityQuestionObj.id,
-                        "QuestionText_EN": securityQuestionObj.question,
-                        "QuestionText_FR": securityQuestionObj.question,
-                        "Active": 1,
-                        "SecurityQuestionSerNum": questionId
-                    }
-                );
-            });
-            return {
-                "data": {
-                    "patientSerNum": patient.patientSerNum,
-                    "securityQuestionWithAnswerList": securityQuestionForLowerVersion,
-                    "activeSecurityQuestionList": activeQuestionForLowerVersion,
-                }
-            };
-        }
-        else{
-            return {
-                "data": {
-                    "patientSerNum": patient.patientSerNum,
-                    "securityQuestionList": securityQuestionList,
-                    "activeSecurityQuestions": activeSecurityQuestions,
-                }
-            };
-        }
+        return {
+            "data": {
+                "patientSerNum": patient.patientSerNum,
+                "securityQuestionList": securityQuestionList,
+                "activeSecurityQuestions": activeSecurityQuestions,
+            }
+        };
     }
 }
 
