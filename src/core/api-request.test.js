c059084dc3193d34d7c901821de8adba72636df8
Stacey Beard <staceybeard10@gmail.com>
2026-03-25 21:26:56 -0400

    Revert "ci: get an example of a coverage failure"
    This reverts commit 593de656960f82a4459a7f7a13fbc74a78e54b3c.

--- src/core/api-request.test.js
+++ src/core/api-request.test.js
@@ -5,8 +5,130 @@
 import '../test/chai-setup.js';
 import ApiRequest from './api-request.js';
 import { expect } from 'chai';
+import { stub } from 'sinon';
+
+const REQUEST = {
+    AppVersion: '100.100.100',
+    Request: 'api',
+    UserID: 'test',
+    Parameters: {
+        method: 'get',
+        url: '/api/test',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept-Language': 'fr',
+        },
+        params: {
+            example: 'param',
+        },
+        data: {
+            example: 'request',
+        },
+    },
+    UserEmail: 'test@opalmedapps.ca',
+};
 
 describe('ApiRequest', function () {
+    describe('makeRequest', function () {
+        let axiosStub;
+
+        before(function () {
+            axiosStub = stub(ApiRequest, 'axios');
+        });
+
+        it('should throw an error given an invalid request', async function () {
+            return expect(ApiRequest.makeRequest({})).to.throw;
+        });
+        it('should throw a BAD_REQUEST error given a 400 response', async function () {
+            axiosStub.throws({
+                code: 'ERR_BAD_REQUEST',
+                headers: {},
+                response: {
+                    status: 400,
+                    statusText: 'Bad Request',
+                },
+            });
+            return expect(ApiRequest.makeRequest(REQUEST)).to.be.rejectedWith('BAD_REQUEST');
+        });
+        it('should throw an API_NOT_FOUND error given a 404 response', async function () {
+            axiosStub.throws({
+                code: 'ERR_BAD_REQUEST',
+                headers: {},
+                response: {
+                    status: 404,
+                    statusText: 'Not Found',
+                },
+            });
+            return expect(ApiRequest.makeRequest(REQUEST)).to.be.rejectedWith('API_NOT_FOUND');
+        });
+        it('should throw an API_UNALLOWED error given a 403 response', async function () {
+            axiosStub.throws({
+                code: 'ERR_BAD_REQUEST',
+                headers: {},
+                response: {
+                    status: 403,
+                    statusText: 'Forbidden',
+                },
+            });
+            return expect(ApiRequest.makeRequest(REQUEST)).to.be.rejectedWith('API_UNALLOWED');
+        });
+        it('should throw an API_NOT_AVAILABLE error given an ECONNREFUSED response', async function () {
+            axiosStub.throws({
+                code: 'ECONNREFUSED',
+                headers: {},
+            });
+            return expect(ApiRequest.makeRequest(REQUEST)).to.be.rejectedWith('API_NOT_AVAILABLE');
+        });
+        it('should throw an API_NOT_AVAILABLE error given an ECONNRESET response', async function () {
+            axiosStub.throws({
+                code: 'ECONNRESET',
+                headers: {},
+            });
+            return expect(ApiRequest.makeRequest(REQUEST)).to.be.rejectedWith('API_NOT_AVAILABLE');
+        });
+        it('should execute successfully given a valid request', async function () {
+            axiosStub.returns({
+                status: 200,
+                statusText: 'OK',
+                headers: {
+                    'content-type': 'application/json',
+                    'content-language': 'en',
+                },
+                data: {
+                    example: 'response',
+                },
+            });
+            const response = await ApiRequest.makeRequest(REQUEST);
+            const requiredProperties = ['status_code', 'headers', 'data'];
+            requiredProperties.forEach(property => {
+                expect(response, 'response is missing a required property').to.have.property(property);
+            });
+        });
+
+        afterEach(function () {
+            axiosStub.resetHistory();
+        });
+
+        after(function () {
+            axiosStub.restore();
+        });
+    });
+
+    describe('sendRequestToApi', function () {
+        it('should throw an error when failing to connect to the backend api', async function () {
+            const userId = 'aksdjhaksd';
+            const parameters = {
+                method: 'get',
+                url: '/api/app/home',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept-Language': 'fr',
+                },
+            };
+            return expect(ApiRequest.sendRequestToApi(userId, parameters)).to.throw;
+        });
+    });
+
     describe('filterOutHTML()', function () {
         it('should filter HTML and return only the text in the summary div', function () {
             return expect(ApiRequest.filterOutHTML(`
